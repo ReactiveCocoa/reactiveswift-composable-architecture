6c272b18e5d3925e93ab4b8a5dd4669bfec29d49
Michael Brown <me@michael-brown.net>
2020-06-12 17:19:46 +0100

    Change the package name to reflect the repo name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "swift-composable-architecture",
+  name: "reactiveswift-composable-architecture",
   platforms: [
     .iOS(.v12),
     .macOS(.v10_14),
