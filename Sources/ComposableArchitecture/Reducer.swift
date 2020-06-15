fc4c90c393cea025425a80a5e2cfff943a10e70e
nmccann <nmccann@users.noreply.github.com>
2020-06-15 10:33:05 -0400

    Aligned documentation with the implementation (#186)
    Co-authored-by: Noah McCann <>

--- Sources/ComposableArchitecture/Reducer.swift
+++ Sources/ComposableArchitecture/Reducer.swift
@@ -55,7 +55,7 @@ public struct Reducer<State, Action, Environment> {
     Self { _, _, _ in .none }
   }
 
-  /// Combines many reducers into a single one by running each one on the state, and concatenating
+  /// Combines many reducers into a single one by running each one on the state, and merging
   /// all of the effects.
   ///
   /// - Parameter reducers: A list of reducers.
@@ -65,7 +65,7 @@ public struct Reducer<State, Action, Environment> {
   }
 
   /// Combines an array of reducers into a single one by running each one on the state, and
-  /// concatenating all of the effects.
+  /// merging all of the effects.
   ///
   /// - Parameter reducers: An array of reducers.
   /// - Returns: A single reducer.
@@ -76,7 +76,7 @@ public struct Reducer<State, Action, Environment> {
   }
 
   /// Combines the current reducer with another given reducer by running each one on the state,
-  /// and concatenating their effects.
+  /// and merging their effects.
   ///
   /// - Parameter other: Another reducer.
   /// - Returns: A single reducer.
