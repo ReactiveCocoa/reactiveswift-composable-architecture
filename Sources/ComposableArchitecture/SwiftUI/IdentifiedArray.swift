5ef9b474bc64c5f13da435583eff9572ad34ffd2
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2020-09-04 06:28:23 +0700

    add reverse implementation (#276)

--- Sources/ComposableArchitecture/SwiftUI/IdentifiedArray.swift
+++ Sources/ComposableArchitecture/SwiftUI/IdentifiedArray.swift
@@ -200,6 +200,10 @@ where ID: Hashable {
     var rng = SystemRandomNumberGenerator()
     self.shuffle(using: &rng)
   }
+
+  public mutating func reverse() {
+    ids.reverse()
+  }
 }
 
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
