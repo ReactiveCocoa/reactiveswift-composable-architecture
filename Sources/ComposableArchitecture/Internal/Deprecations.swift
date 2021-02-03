9cd2649abc9e071e2c84d1aada86e84ae2faa409
mluisbrown <mluisbrown@users.noreply.github.com>
2021-02-03 12:47:09 +0000

    Run swift-format

--- Sources/ComposableArchitecture/Internal/Deprecations.swift
+++ Sources/ComposableArchitecture/Internal/Deprecations.swift
@@ -1,88 +1,88 @@
 #if canImport(SwiftUI)
 
-import SwiftUI
+  import SwiftUI
 
-// NB: Deprecated after 0.10.0:
+  // NB: Deprecated after 0.10.0:
 
-@available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
-extension ActionSheetState {
-  @available(*, deprecated, message: "'title' and 'message' should be 'TextState'")
-  @_disfavoredOverload
-  public init(
-    title: LocalizedStringKey,
-    message: LocalizedStringKey? = nil,
-    buttons: [Button]
-  ) {
-    self.init(
-      title: .init(title),
-      message: message.map { .init($0) },
-      buttons: buttons
-    )
-  }
-}
-
-@available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
-extension AlertState {
-  @available(*, deprecated, message: "'title' and 'message' should be 'TextState'")
-  @_disfavoredOverload
-  public init(
-    title: LocalizedStringKey,
-    message: LocalizedStringKey? = nil,
-    dismissButton: Button? = nil
-  ) {
-    self.init(
-      title: .init(title),
-      message: message.map { .init($0) },
-      dismissButton: dismissButton
-    )
+  @available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
+  extension ActionSheetState {
+    @available(*, deprecated, message: "'title' and 'message' should be 'TextState'")
+    @_disfavoredOverload
+    public init(
+      title: LocalizedStringKey,
+      message: LocalizedStringKey? = nil,
+      buttons: [Button]
+    ) {
+      self.init(
+        title: .init(title),
+        message: message.map { .init($0) },
+        buttons: buttons
+      )
+    }
   }
 
-  @available(*, deprecated, message: "'title' and 'message' should be 'TextState'")
-  @_disfavoredOverload
-  public init(
-    title: LocalizedStringKey,
-    message: LocalizedStringKey? = nil,
-    primaryButton: Button,
-    secondaryButton: Button
-  ) {
-    self.init(
-      title: .init(title),
-      message: message.map { .init($0) },
-      primaryButton: primaryButton,
-      secondaryButton: secondaryButton
-    )
-  }
-}
+  @available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
+  extension AlertState {
+    @available(*, deprecated, message: "'title' and 'message' should be 'TextState'")
+    @_disfavoredOverload
+    public init(
+      title: LocalizedStringKey,
+      message: LocalizedStringKey? = nil,
+      dismissButton: Button? = nil
+    ) {
+      self.init(
+        title: .init(title),
+        message: message.map { .init($0) },
+        dismissButton: dismissButton
+      )
+    }
 
-@available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
-extension AlertState.Button {
-  @available(*, deprecated, message: "'label' should be 'TextState'")
-  @_disfavoredOverload
-  public static func cancel(
-    _ label: LocalizedStringKey,
-    send action: Action? = nil
-  ) -> Self {
-    Self(action: action, type: .cancel(label: .init(label)))
-  }
-
-  @available(*, deprecated, message: "'label' should be 'TextState'")
-  @_disfavoredOverload
-  public static func `default`(
-    _ label: LocalizedStringKey,
-    send action: Action? = nil
-  ) -> Self {
-    Self(action: action, type: .default(label: .init(label)))
+    @available(*, deprecated, message: "'title' and 'message' should be 'TextState'")
+    @_disfavoredOverload
+    public init(
+      title: LocalizedStringKey,
+      message: LocalizedStringKey? = nil,
+      primaryButton: Button,
+      secondaryButton: Button
+    ) {
+      self.init(
+        title: .init(title),
+        message: message.map { .init($0) },
+        primaryButton: primaryButton,
+        secondaryButton: secondaryButton
+      )
+    }
   }
 
-  @available(*, deprecated, message: "'label' should be 'TextState'")
-  @_disfavoredOverload
-  public static func destructive(
-    _ label: LocalizedStringKey,
-    send action: Action? = nil
-  ) -> Self {
-    Self(action: action, type: .destructive(label: .init(label)))
+  @available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
+  extension AlertState.Button {
+    @available(*, deprecated, message: "'label' should be 'TextState'")
+    @_disfavoredOverload
+    public static func cancel(
+      _ label: LocalizedStringKey,
+      send action: Action? = nil
+    ) -> Self {
+      Self(action: action, type: .cancel(label: .init(label)))
+    }
+
+    @available(*, deprecated, message: "'label' should be 'TextState'")
+    @_disfavoredOverload
+    public static func `default`(
+      _ label: LocalizedStringKey,
+      send action: Action? = nil
+    ) -> Self {
+      Self(action: action, type: .default(label: .init(label)))
+    }
+
+    @available(*, deprecated, message: "'label' should be 'TextState'")
+    @_disfavoredOverload
+    public static func destructive(
+      _ label: LocalizedStringKey,
+      send action: Action? = nil
+    ) -> Self {
+      Self(action: action, type: .destructive(label: .init(label)))
+    }
   }
-}
 #endif
 
 // NB: Deprecated after 0.9.0:
--- Tests/ComposableArchitectureTests/Internal/LocalizedStringTests.swift
+++ Tests/ComposableArchitectureTests/Internal/LocalizedStringTests.swift
@@ -6,11 +6,11 @@
 
   @available(iOS 13.0, macOS 10.15, macCatalyst 13, tvOS 13.0, watchOS 6.0, *)
   class LocalizedStringTests: XCTestCase {
-  func testLocalizedStringKeyFormatting() {
-    XCTAssertEqual(
-      LocalizedStringKey("Hello, #\(42)!").formatted(),
-      "Hello, #42!"
-    )
+    func testLocalizedStringKeyFormatting() {
+      XCTAssertEqual(
+        LocalizedStringKey("Hello, #\(42)!").formatted(),
+        "Hello, #42!"
+      )
 
       let formatter = NumberFormatter()
       formatter.locale = Locale(identifier: "en_US")
