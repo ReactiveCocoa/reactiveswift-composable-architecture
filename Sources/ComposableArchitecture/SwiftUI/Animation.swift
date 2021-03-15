04431adf0bef111784a8bff13dae6663bfdec56a
Michael Brown <me@michael-brown.net>
2021-03-15 18:16:54 +0000

    Add extensions to ReactiveSwift Schedulers to support the `animation` and `transaction` modifiers.

--- Sources/ComposableArchitecture/SwiftUI/Animation.swift
+++ Sources/ComposableArchitecture/SwiftUI/Animation.swift
@@ -1,3 +1,4 @@
+#if canImport(SwiftUI)
 import SwiftUI
 
 extension ViewStore {
@@ -6,9 +7,11 @@ extension ViewStore {
   /// - Parameters:
   ///   - action: An action.
   ///   - animation: An animation.
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
   public func send(_ action: Action, animation: Animation?) {
     withAnimation(animation) {
       self.send(action)
     }
   }
 }
+#endif
