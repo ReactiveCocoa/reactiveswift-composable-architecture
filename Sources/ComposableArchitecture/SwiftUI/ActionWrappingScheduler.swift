5e73dc3a53ee6d891396dd0d9167a6807a3a08c2
Michael Brown <me@michael-brown.net>
2021-03-15 18:16:54 +0000

    Add extensions to ReactiveSwift Schedulers to support the `animation` and `transaction` modifiers.

---
+++ Sources/ComposableArchitecture/SwiftUI/ActionWrappingScheduler.swift
@@ -0,0 +1,120 @@
+#if canImport(SwiftUI)
+import SwiftUI
+import ReactiveSwift
+
+extension Scheduler {
+  /// Specifies an animation to perform when an action is scheduled.
+  ///
+  /// - Parameter animation: An animation to be performed.
+  /// - Returns: A scheduler that performs an animation when a scheduled action is run.
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+  public func animation(_ animation: Animation? = .default) -> Scheduler {
+    ActionWrappingScheduler(scheduler: self, wrapper: .animation(animation))
+  }
+
+  /// Wraps scheduled actions in a transaction.
+  ///
+  /// - Parameter transaction: A transaction.
+  /// - Returns: A scheduler that wraps scheduled actions in a transaction.
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+  public func transaction(_ transaction: Transaction) -> Scheduler {
+    ActionWrappingScheduler(scheduler: self, wrapper: .transaction(transaction))
+  }
+}
+
+extension DateScheduler {
+  /// Specifies an animation to perform when an action is scheduled.
+  ///
+  /// - Parameter animation: An animation to be performed.
+  /// - Returns: A scheduler that performs an animation when a scheduled action is run.
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+  public func animation(_ animation: Animation? = .default) -> DateScheduler {
+    ActionWrappingDateScheduler(scheduler: self, wrapper: .animation(animation))
+  }
+
+  /// Wraps scheduled actions in a transaction.
+  ///
+  /// - Parameter transaction: A transaction.
+  /// - Returns: A scheduler that wraps scheduled actions in a transaction.
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+  public func transaction(_ transaction: Transaction) -> DateScheduler {
+    ActionWrappingDateScheduler(scheduler: self, wrapper: .transaction(transaction))
+  }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+private enum ActionWrapper {
+  case animation(Animation?)
+  case transaction(Transaction)
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public final class ActionWrappingScheduler: Scheduler {
+  private let scheduler: Scheduler
+  private let wrapper: ActionWrapper
+
+  fileprivate init(scheduler: Scheduler, wrapper: ActionWrapper) {
+    self.scheduler = scheduler
+    self.wrapper = wrapper
+  }
+
+  public func schedule(_ action: @escaping () -> Void) -> Disposable? {
+    scheduler.schedule {
+      switch self.wrapper {
+      case let .animation(animation):
+        withAnimation(animation, action)
+      case let .transaction(transaction):
+        withTransaction(transaction, action)
+      }
+    }
+  }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public final class ActionWrappingDateScheduler: DateScheduler {
+  public var currentDate: Date {
+    scheduler.currentDate
+  }
+
+  private let scheduler: DateScheduler
+  private let wrapper: ActionWrapper
+
+  fileprivate init(scheduler: DateScheduler, wrapper: ActionWrapper) {
+    self.scheduler = scheduler
+    self.wrapper = wrapper
+  }
+
+  public func schedule(_ action: @escaping () -> Void) -> Disposable? {
+    scheduler.schedule {
+      switch self.wrapper {
+      case let .animation(animation):
+        withAnimation(animation, action)
+      case let .transaction(transaction):
+        withTransaction(transaction, action)
+      }
+    }
+  }
+
+  public func schedule(after date: Date, action: @escaping () -> Void) -> Disposable? {
+    scheduler.schedule(after: date) {
+      switch self.wrapper {
+      case let .animation(animation):
+        withAnimation(animation, action)
+      case let .transaction(transaction):
+        withTransaction(transaction, action)
+      }
+    }
+  }
+
+  public func schedule(after date: Date, interval: DispatchTimeInterval, leeway: DispatchTimeInterval, action: @escaping () -> Void) -> Disposable? {
+    scheduler.schedule(after: date, interval: interval, leeway: leeway) {
+      switch self.wrapper {
+      case let .animation(animation):
+        withAnimation(animation, action)
+      case let .transaction(transaction):
+        withTransaction(transaction, action)
+      }
+    }
+  }
+}
+#endif
