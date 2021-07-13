42262c4caafefa116ef0a28792fc69bf2a93b524
Michael Brown <me@michael-brown.net>
2021-07-13 22:59:34 +0100

    Run swift format

--- Tests/ComposableArchitectureTests/EffectDeferredTests.swift
+++ Tests/ComposableArchitectureTests/EffectDeferredTests.swift
@@ -6,50 +6,50 @@ final class EffectDeferredTests: XCTestCase {
   func testDeferred() {
     let scheduler = TestScheduler()
     var values: [Int] = []
-    
+
     func runDeferredEffect(value: Int) {
       SignalProducer(value: value)
         .deferred(for: 1, scheduler: scheduler)
         .startWithValues { values.append($0) }
     }
-    
+
     runDeferredEffect(value: 1)
-    
+
     // Nothing emits right away.
     XCTAssertEqual(values, [])
-    
+
     // Waiting half the time also emits nothing
     scheduler.advance(by: .milliseconds(500))
     XCTAssertEqual(values, [])
-    
+
     // Run another deferred effect.
     runDeferredEffect(value: 2)
-    
+
     // Waiting half the time emits first deferred effect received.
     scheduler.advance(by: .milliseconds(500))
     XCTAssertEqual(values, [1])
-    
+
     // Run another deferred effect.
     runDeferredEffect(value: 3)
-    
+
     // Waiting half the time emits second deferred effect received.
     scheduler.advance(by: .milliseconds(500))
     XCTAssertEqual(values, [1, 2])
-    
+
     // Waiting the rest of the time emits the final effect value.
     scheduler.advance(by: .milliseconds(500))
     XCTAssertEqual(values, [1, 2, 3])
-    
+
     // Running out the scheduler
     scheduler.run()
     XCTAssertEqual(values, [1, 2, 3])
   }
-  
+
   func testDeferredIsLazy() {
     let scheduler = TestScheduler()
     var values: [Int] = []
     var effectRuns = 0
-    
+
     func runDeferredEffect(value: Int) {
       Effect.deferred { () -> Effect<Int, Never> in
         effectRuns += 1
@@ -58,19 +58,19 @@ final class EffectDeferredTests: XCTestCase {
       .deferred(for: 1, scheduler: scheduler)
       .startWithValues { values.append($0) }
     }
-    
+
     runDeferredEffect(value: 1)
-    
+
     XCTAssertEqual(values, [])
     XCTAssertEqual(effectRuns, 0)
-    
+
     scheduler.advance(by: .milliseconds(500))
-    
+
     XCTAssertEqual(values, [])
     XCTAssertEqual(effectRuns, 0)
-    
+
     scheduler.advance(by: .milliseconds(500))
-    
+
     XCTAssertEqual(values, [1])
     XCTAssertEqual(effectRuns, 1)
   }
