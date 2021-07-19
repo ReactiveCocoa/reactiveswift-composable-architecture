12e99a90a361f6f7d1ad364f84f86bc9195e04fe
Michael Brown <me@michael-brown.net>
2021-07-19 12:12:33 +0100

    Bump swift-tools-version to 5.3 Bump CasPaths to use from 0.4.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -19,7 +19,7 @@ let package = Package(
   dependencies: [
     .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
     .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", from: "6.6.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.1.3"),
+    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.4.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0"),
   ],
   targets: [
