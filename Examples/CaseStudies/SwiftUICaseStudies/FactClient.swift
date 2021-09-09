47eaa7bd14be9cc43dcc60d31f40116a8a90b9f6
Michael Brown <me@michael-brown.net>
2021-09-09 16:33:15 +0100

    Add `Effect.task` for wrapping units of async/await work (#715)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * docs
    
    * clean up
    
    * Remove bad test
    
    Co-authored-by: Brandon Williams <mbrandonw@hey.com>

--- Examples/CaseStudies/SwiftUICaseStudies/FactClient.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/FactClient.swift
@@ -9,22 +9,41 @@ struct FactClient {
   struct Error: Swift.Error, Equatable {}
 }
 
-extension FactClient {
   // This is the "live" fact dependency that reaches into the outside world to fetch trivia.
   // Typically this live implementation of the dependency would live in its own module so that the
   // main feature doesn't need to compile it.
+extension FactClient {
+  #if compiler(>=5.5)
   static let live = Self(
     fetch: { number in
-      URLSession.shared.reactive.data(
-        with: URLRequest(url: URL(string: "http://numbersapi.com/\(number)/trivia")!)
-      )
-      .map { data, _ in String(decoding: data, as: UTF8.self) }
-      .flatMapError { _ in
-        Effect(value: "\(number) is a good number Brent")
-          .delay(1, on: QueueScheduler.main)
+        Effect.task {
+          do {
+            let (data, _) = try await URLSession.shared
+              .data(from: URL(string: "http://numbersapi.com/\(number)/trivia")!)
+            return String(decoding: data, as: UTF8.self)
+          } catch {
+            await Task.sleep(NSEC_PER_SEC)
+            return "\(number) is a good number Brent"
+          }
+        }
+        .setFailureType(to: Error.self)
+        .eraseToEffect()
       }
-      .promoteError(Error.self)
-    })
+    )
+  #else
+    static let live = Self(
+      fetch: { number in
+        URLSession.shared.reactive.data(
+          with: URLRequest(url: URL(string: "http://numbersapi.com/\(number)/trivia")!)
+        )
+        .map { data, _ in String(decoding: data, as: UTF8.self) }
+        .flatMapError { _ in
+          Effect(value: "\(number) is a good number Brent")
+            .delay(1, on: QueueScheduler.main)
+        }
+        .promoteError(Error.self)
+      })
+  #endif
 }
 
 #if DEBUG
