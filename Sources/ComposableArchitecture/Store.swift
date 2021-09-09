0cf3727fd35627e5853115e6f80e89c54ed8aae7
Michael Brown <me@michael-brown.net>
2021-09-09 22:16:48 +0100

    Update to thread warnings. (#773)
    * Update to thread warnings.
    
    * fix tests
    
    * clean up
    
    * rename variable

--- Sources/ComposableArchitecture/Store.swift
+++ Sources/ComposableArchitecture/Store.swift
@@ -129,6 +129,9 @@ public final class Store<State, Action> {
   private var parentDisposable: Disposable?
   private let reducer: (inout State, Action) -> Effect<Action, Never>
   private var bufferedActions: [Action] = []
+  #if DEBUG
+  private var initialThread = Thread.current
+  #endif
 
   /// Initializes a store from an initial state, a reducer, and an environment.
   ///
@@ -372,7 +375,9 @@ public final class Store<State, Action> {
     self.producerScope(state: toLocalState, action: { $0 })
   }
 
-  func send(_ action: Action) {
+  func send(_ action: Action, isFromViewStore: Bool = true) {
+    self.threadCheck(status: .send(action, isFromViewStore: isFromViewStore))
+
     self.bufferedActions.append(action)
     guard !self.isSending else { return }
 
@@ -389,43 +394,12 @@ public final class Store<State, Action> {
 
       var didComplete = false
       let uuid = UUID()
-
-      #if DEBUG
-      let initalThread = Thread.current
-      initalThread.threadDictionary[uuid] = true
-      #endif
-
       let observer = Signal<Action, Never>.Observer(
         value: { [weak self] action in
-          self?.send(action)
+          self?.send(action, isFromViewStore: false)
         },
         completed: { [weak self] in
-          #if DEBUG
-          if Thread.current.threadDictionary[uuid] == nil {
-            breakpoint(
-            """
-            ---
-            Warning: Store.send
-
-            The Store class is not thread-safe, and so all interactions with an instance of Store
-            (including all of its scopes and derived ViewStores) must be done on the same thread.
-
-            \(debugCaseOutput(action)) has produced an Effect that was completed on a different thread \
-            from the one it was executed on.
-              
-            Starting thread: \(initalThread)
-            Final thread: \(Thread.current)
-              
-            Possible fixes for this are:
-
-            * Add a .receive(on:) to the Effect to ensure it completes on this Stores correct thread.
-            """
-            )
-          }
-
-          Thread.current.threadDictionary[uuid] = nil
-          #endif
-
+          self?.threadCheck(status: .effectCompletion(action))
           didComplete = true
           self?.effectDisposables.removeValue(forKey: uuid)?.dispose()
         },
@@ -458,5 +432,60 @@ public final class Store<State, Action> {
     self.effectDisposables.keys.forEach { id in
       self.effectDisposables.removeValue(forKey: id)?.dispose()
     }
+  }
+
+  private enum ThreadCheckStatus {
+    case effectCompletion(Action)
+    case send(Action, isFromViewStore: Bool)
+  }
+
+  @inline(__always)
+  private func threadCheck(status: ThreadCheckStatus) {
+    #if DEBUG
+    guard self.initialThread != Thread.current
+    else { return }
+
+    let message: String
+    switch status {
+    case let .effectCompletion(action):
+      message = """
+        An effect returned from the action "\(debugCaseOutput(action))" completed on the \
+        wrong thread. Make sure to use ".receive(on:)" on any effects that execute on background \
+        threads to receive their output on the initial thread.
+        """
+
+    case let .send(action, isFromViewStore: true):
+      message = """
+        "ViewStore.send(\(debugCaseOutput(action)))" was called on the wrong thread. Make \
+        sure that "ViewStore.send" is always called on the initial thread.
+        """
+
+    case let .send(action, isFromViewStore: false):
+      message = """
+        An effect emitted the action "\(debugCaseOutput(action))" from the wrong thread. Make sure \
+        to use ".receive(on:)" on any effects that execute on background threads to receive their \
+        output on the initial thread.
+        """
+    }
+
+    breakpoint(
+      """
+      ---
+      Warning:
+
+      The store was interacted with on a thread that is different from the thread the store was \
+      created on:
+
+      \(message)
+
+        Initial thread: \(self.initialThread)
+        Current thread: \(Thread.current)
+
+      The "Store" class is not thread-safe, and so all interactions with an instance of "Store" \
+      (including all of its scopes and derived view stores) must be done on the same thread.
+      ---
+      """
+    )
+    #endif
   }
 }
--- Tests/ComposableArchitectureTests/DebugTests.swift
+++ Tests/ComposableArchitectureTests/DebugTests.swift
@@ -348,7 +348,7 @@ final class DebugTests: XCTestCase {
 
   #if canImport(SwiftUI)
     @available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
-    func testTextState() {
+  func testTextState() {
       XCTAssertEqual(
         debugOutput(
           TextState("Hello, world!")
@@ -358,12 +358,12 @@ final class DebugTests: XCTestCase {
           Hello, world!
         )
         """
-      )
+    )
 
       XCTAssertEqual(
         debugOutput(
-          TextState("Hello, ")
-            + TextState("world").bold().italic()
+      TextState("Hello, ")
+        + TextState("world").bold().italic()
             + TextState("!")
         ),
         """
@@ -371,47 +371,47 @@ final class DebugTests: XCTestCase {
           Hello, _**world**_!
         )
         """
-      )
+    )
 
       XCTAssertEqual(
         debugOutput(
-          TextState("Offset by 10.5").baselineOffset(10.5)
-            + TextState("\n") + TextState("Headline").font(.headline)
-            + TextState("\n") + TextState("No font").font(nil)
-            + TextState("\n") + TextState("Light font weight").fontWeight(.light)
-            + TextState("\n") + TextState("No font weight").fontWeight(nil)
-            + TextState("\n") + TextState("Red").foregroundColor(.red)
-            + TextState("\n") + TextState("No color").foregroundColor(nil)
-            + TextState("\n") + TextState("Italic").italic()
-            + TextState("\n") + TextState("Kerning of 2.5").kerning(2.5)
-            + TextState("\n") + TextState("Stricken").strikethrough()
-            + TextState("\n") + TextState("Stricken green").strikethrough(color: .green)
-            + TextState("\n") + TextState("Not stricken blue").strikethrough(false, color: .blue)
-            + TextState("\n") + TextState("Tracking of 5.5").tracking(5.5)
-            + TextState("\n") + TextState("Underlined").underline()
-            + TextState("\n") + TextState("Underlined pink").underline(color: .pink)
+      TextState("Offset by 10.5").baselineOffset(10.5)
+        + TextState("\n") + TextState("Headline").font(.headline)
+        + TextState("\n") + TextState("No font").font(nil)
+        + TextState("\n") + TextState("Light font weight").fontWeight(.light)
+        + TextState("\n") + TextState("No font weight").fontWeight(nil)
+        + TextState("\n") + TextState("Red").foregroundColor(.red)
+        + TextState("\n") + TextState("No color").foregroundColor(nil)
+        + TextState("\n") + TextState("Italic").italic()
+        + TextState("\n") + TextState("Kerning of 2.5").kerning(2.5)
+        + TextState("\n") + TextState("Stricken").strikethrough()
+        + TextState("\n") + TextState("Stricken green").strikethrough(color: .green)
+        + TextState("\n") + TextState("Not stricken blue").strikethrough(false, color: .blue)
+        + TextState("\n") + TextState("Tracking of 5.5").tracking(5.5)
+        + TextState("\n") + TextState("Underlined").underline()
+        + TextState("\n") + TextState("Underlined pink").underline(color: .pink)
             + TextState("\n") + TextState("Not underlined purple").underline(false, color: .pink)
         ),
-        """
-        TextState(
-          <baseline-offset=10.5>Offset by 10.5</baseline-offset>
-          Headline
-          No font
-          <font-weight=light>Light font weight</font-weight>
-          No font weight
-          <foreground-color=red>Red</foreground-color>
-          No color
-          _Italic_
-          <kerning=2.5>Kerning of 2.5</kerning>
-          ~~Stricken~~
-          <s color=green>Stricken green</s>
-          Not stricken blue
-          <tracking=5.5>Tracking of 5.5</tracking>
-          <u>Underlined</u>
-          <u color=pink>Underlined pink</u>
-          Not underlined purple
-        )
-        """
+      """
+      TextState(
+        <baseline-offset=10.5>Offset by 10.5</baseline-offset>
+        Headline
+        No font
+        <font-weight=light>Light font weight</font-weight>
+        No font weight
+        <foreground-color=red>Red</foreground-color>
+        No color
+        _Italic_
+        <kerning=2.5>Kerning of 2.5</kerning>
+        ~~Stricken~~
+        <s color=green>Stricken green</s>
+        Not stricken blue
+        <tracking=5.5>Tracking of 5.5</tracking>
+        <u>Underlined</u>
+        <u color=pink>Underlined pink</u>
+        Not underlined purple
+      )
+      """
       )
     }
   #endif
@@ -956,6 +956,24 @@ final class DebugTests: XCTestCase {
             query: "Hi"
           )
         )
+      )
+      """
+    )
+  }
+
+  func testBindingAction() {
+    struct State {
+      @BindableState var width = 0
+    }
+
+    XCTAssertEqual(
+      debugOutput(BindingAction.set(\State.$width, 50)),
+      """
+      BindingAction<State>(
+        keyPath: WritableKeyPath<State, BindableState<Int>>(),
+        set: (Function),
+        value: 50,
+        valueIsEqualTo: (Function)
       )
       """
     )
