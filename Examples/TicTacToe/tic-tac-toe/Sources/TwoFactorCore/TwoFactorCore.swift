8f0069dc674b35e65daa30201ce420a8a8fbd32c
Michael Brown <me@michael-brown.net>
2021-09-09 13:52:01 +0100

    tic-tac-toe package (#684)
    * tic-tac-toe package
    
    * Update Package.swift
    
    * Tests
    
    * wip
    
    * wip
    
    * wip
    
    * Update Package.swift

---
+++ Examples/TicTacToe/tic-tac-toe/Sources/TwoFactorCore/TwoFactorCore.swift
@@ -0,0 +1,77 @@
+import AuthenticationClient
+import ComposableArchitecture
+import Dispatch
+<<<<<<< ours:Examples/TicTacToe/Sources/Core/TwoFactorCore.swift
+import ReactiveSwift
+import TicTacToeCommon
+=======
+>>>>>>> theirs:Examples/TicTacToe/tic-tac-toe/Sources/TwoFactorCore/TwoFactorCore.swift
+
+public struct TwoFactorState: Equatable {
+  public var alert: AlertState<TwoFactorAction>?
+  public var code = ""
+  public var isFormValid = false
+  public var isTwoFactorRequestInFlight = false
+  public let token: String
+
+  public init(token: String) {
+    self.token = token
+  }
+}
+
+public enum TwoFactorAction: Equatable {
+  case alertDismissed
+  case codeChanged(String)
+  case submitButtonTapped
+  case twoFactorResponse(Result<AuthenticationResponse, AuthenticationError>)
+}
+
+public struct TwoFactorTearDownToken: Hashable {
+  public init() {}
+}
+
+public struct TwoFactorEnvironment {
+  public var authenticationClient: AuthenticationClient
+  public var mainQueue: DateScheduler
+
+  public init(
+    authenticationClient: AuthenticationClient,
+    mainQueue: DateScheduler
+  ) {
+    self.authenticationClient = authenticationClient
+    self.mainQueue = mainQueue
+  }
+}
+
+public let twoFactorReducer = Reducer<TwoFactorState, TwoFactorAction, TwoFactorEnvironment> {
+  state, action, environment in
+
+  switch action {
+  case .alertDismissed:
+    state.alert = nil
+    return .none
+
+  case let .codeChanged(code):
+    state.code = code
+    state.isFormValid = code.count >= 4
+    return .none
+
+  case .submitButtonTapped:
+    state.isTwoFactorRequestInFlight = true
+    return environment.authenticationClient
+      .twoFactor(TwoFactorRequest(code: state.code, token: state.token))
+      .observe(on: environment.mainQueue)
+      .catchToEffect()
+      .map(TwoFactorAction.twoFactorResponse)
+      .cancellable(id: TwoFactorTearDownToken())
+
+  case let .twoFactorResponse(.failure(error)):
+    state.alert = .init(title: TextState(error.localizedDescription))
+    state.isTwoFactorRequestInFlight = false
+    return .none
+
+  case let .twoFactorResponse(.success(response)):
+    state.isTwoFactorRequestInFlight = false
+    return .none
+  }
+}
