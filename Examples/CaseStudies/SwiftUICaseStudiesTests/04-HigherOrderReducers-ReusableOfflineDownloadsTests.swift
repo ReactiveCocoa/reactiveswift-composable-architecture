df3625a8084b73fc7078cc9367704babebecb74b
Michael Brown <me@michael-brown.net>
2021-09-09 11:49:36 +0100

    Update AlertState.Button APIs for animation (#681)
    * Update AlertState.Button APIs for animation
    
    * fix
    
    * wip
    
    * deprecate

--- Examples/CaseStudies/SwiftUICaseStudiesTests/04-HigherOrderReducers-ReusableOfflineDownloadsTests.swift
+++ Examples/CaseStudies/SwiftUICaseStudiesTests/04-HigherOrderReducers-ReusableOfflineDownloadsTests.swift
@@ -120,8 +120,8 @@ class ReusableComponentsDownloadComponentTests: XCTestCase {
     store.send(.buttonTapped) {
       $0.alert = .init(
         title: .init("Do you want to cancel downloading this map?"),
-        primaryButton: .destructive(.init("Cancel"), send: .cancelButtonTapped),
-        secondaryButton: .default(.init("Nevermind"), send: .nevermindButtonTapped)
+        primaryButton: .destructive(.init("Cancel"), action: .send(.cancelButtonTapped)),
+        secondaryButton: .default(.init("Nevermind"), action: .send(.nevermindButtonTapped))
       )
     }
 
@@ -160,8 +160,8 @@ class ReusableComponentsDownloadComponentTests: XCTestCase {
     store.send(.buttonTapped) {
       $0.alert = .init(
         title: .init("Do you want to cancel downloading this map?"),
-        primaryButton: .destructive(.init("Cancel"), send: .cancelButtonTapped),
-        secondaryButton: .default(.init("Nevermind"), send: .nevermindButtonTapped)
+        primaryButton: .destructive(.init("Cancel"), action: .send(.cancelButtonTapped)),
+        secondaryButton: .default(.init("Nevermind"), action: .send(.nevermindButtonTapped))
       )
     }
 
@@ -198,8 +198,8 @@ class ReusableComponentsDownloadComponentTests: XCTestCase {
     store.send(.buttonTapped) {
       $0.alert = .init(
         title: .init("Do you want to delete this map from your offline storage?"),
-        primaryButton: .destructive(.init("Delete"), send: .deleteButtonTapped),
-        secondaryButton: .default(.init("Nevermind"), send: .nevermindButtonTapped)
+        primaryButton: .destructive(.init("Delete"), action: .send(.deleteButtonTapped)),
+        secondaryButton: .default(.init("Nevermind"), action: .send(.nevermindButtonTapped))
       )
     }
 
