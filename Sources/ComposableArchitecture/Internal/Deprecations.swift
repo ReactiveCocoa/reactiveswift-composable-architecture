c95f74a9a9567ca290c9ac937b5b31cba31f1508
Michael Brown <me@michael-brown.net>
2021-11-09 22:55:15 +0000

    Deprecate Store.publisherScope (#758)

--- Sources/ComposableArchitecture/Internal/Deprecations.swift
+++ Sources/ComposableArchitecture/Internal/Deprecations.swift
@@ -5,6 +5,70 @@ import SwiftUI
 
 // NB: Deprecated after 0.27.1:
 
+extension Store {
+  /// Scopes the store to a producer of stores of more local state and local actions.
+  ///
+  /// - Parameters:
+  ///   - toLocalState: A function that transforms a producer of `State` into a producer of
+  ///     `LocalState`.
+  ///   - fromLocalAction: A function that transforms `LocalAction` into `Action`.
+  /// - Returns: A producer of stores with its domain (state and action) transformed.
+  @available(
+    *, deprecated,
+    message:
+      "If you use this method, please open a discussion on GitHub and let us know how: https://github.com/pointfreeco/swift-composable-architecture/discussions/new"
+  )
+  public func producerScope<LocalState, LocalAction>(
+    state toLocalState: @escaping (Effect<State, Never>) -> Effect<LocalState, Never>,
+    action fromLocalAction: @escaping (LocalAction) -> Action
+  ) -> Effect<Store<LocalState, LocalAction>, Never> {
+
+    func extractLocalState(_ state: State) -> LocalState? {
+      var localState: LocalState?
+      _ = toLocalState(Effect(value: state))
+        .startWithValues { localState = $0 }
+      return localState
+    }
+
+    return toLocalState(self.producer)
+      .map { localState in
+        let localStore = Store<LocalState, LocalAction>(
+          initialState: localState,
+          reducer: .init { localState, localAction, _ in
+            self.send(fromLocalAction(localAction))
+            localState = extractLocalState(self.state) ?? localState
+            return .none
+          },
+          environment: ()
+        )
+        localStore.parentDisposable = self.producer.startWithValues {
+          [weak localStore] state in
+          guard let localStore = localStore else { return }
+          localStore.state = extractLocalState(state) ?? localStore.state
+        }
+        return localStore
+      }
+  }
+
+  /// Scopes the store to a producer of stores of more local state and local actions.
+  ///
+  /// - Parameter toLocalState: A function that transforms a producer of `State` into a producer
+  ///   of `LocalState`.
+  /// - Returns: A producer of stores with its domain (state and action)
+  ///   transformed.
+  @available(
+    *, deprecated,
+    message:
+      "If you use this method, please open a discussion on GitHub and let us know how: https://github.com/pointfreeco/swift-composable-architecture/discussions/new"
+  )
+  public func producerScope<LocalState>(
+    state toLocalState: @escaping (Effect<State, Never>) -> Effect<LocalState, Never>
+  ) -> Effect<Store<LocalState, Action>, Never> {
+    self.producerScope(state: toLocalState, action: { $0 })
+  }
+
+}
+
 extension ViewStore {
   @available(
     *, deprecated,
