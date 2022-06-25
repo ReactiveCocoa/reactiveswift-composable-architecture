1b866ecc912d47563b33e8331153edec808e4f7b
Michael Brown <me@michael-brown.net>
2022-06-25 21:48:18 +0100

    Conform `WithViewStore` to `Commands` (#1113)
    * Conform `WithViewStore` to `Commands`
    
    * Fix typo
    
    * Add coverage for `Commands`
    
    * Fix documentation
    
    * Conform `WithViewStore` to `AccessibilityRotorContent` and `ToolbarContent`
    
    * Conform `WithViewStore` to `TableColumnContent`
    
    * Fix indentation
    
    * Conform `WithViewStore` to `TableRowContent`
    
    * Fix Typo
    
    * Conform `WithViewStore` to `DynamicTableRowContent`
    
    * Reorder declarations according to the protocol name
    
    * Add coverage for `WithViewStore` new conformances
    
    * Remove Table-related conformances
    
    * Rearrange extensions for better autocomplete.
    
    Co-authored-by: Brandon Williams <mbrandonw@hey.com>

--- Tests/ComposableArchitectureTests/WithViewStoreAppTest.swift
+++ Tests/ComposableArchitectureTests/WithViewStoreAppTest.swift
@@ -1,40 +1,63 @@
-// NB: This file gathers coverage of `WithViewStore` use as a `Scene`.
+// NB: This file gathers coverage of various `WithViewStore` conformances.
 #if canImport(SwiftUI)
+import ComposableArchitecture
+import SwiftUI
 
-  import ComposableArchitecture
-  import SwiftUI
+@available(iOS 14, macOS 11, tvOS 14, watchOS 7, *)
+struct TestApp: App {
+  @Namespace var namespace
+  
+  let store = Store(
+    initialState: 0,
+    reducer: Reducer<Int, Void, Void> { state, _, _ in
+      state += 1
+      return .none
+    },
+    environment: ()
+  )
 
-  @available(iOS 14, macOS 11, tvOS 14, watchOS 7, *)
-  struct TestApp: App {
-    let store = Store(
-      initialState: 0,
-      reducer: Reducer<Int, Void, Void> { state, _, _ in
-        state += 1
-        return .none
-      },
-      environment: ()
-    )
-
-    var body: some Scene {
+  var body: some Scene {
+    WithViewStore(self.store) { viewStore in
+        WindowGroup {
+        checkAccessibilityRotor()
+        checkToolbar()
+      }
+        }
+    #if os(iOS) || os(macOS)
+        .commands {
       WithViewStore(self.store) { viewStore in
-        #if os(iOS) || os(macOS)
-          WindowGroup {
-            EmptyView()
-          }
-          .commands {
-            CommandMenu("Commands") {
-              Button("Increment") {
-                viewStore.send(())
-              }
-              .keyboardShortcut("+")
+          CommandMenu("Commands") {
+            Button("Increment") {
+              viewStore.send(())
             }
           }
-        #else
-          WindowGroup {
-            EmptyView()
-          }
-        #endif
-      }
+        }
+        }
+      #endif
     }
+  
+  @ViewBuilder
+  func checkToolbar() -> some View {
+    Color.clear
+      .toolbar {
+        WithViewStore(store) { viewStore in
+          ToolbarItem {
+            Button(action: { viewStore.send(()) }, label: { Text("Increment") })
+          }
+        }
+      }
   }
+
+  @ViewBuilder
+  func checkAccessibilityRotor() -> some View {
+    if #available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *) {
+      Color.clear
+        .accessibilityRotor("Rotor") {
+          WithViewStore(store) { viewStore in
+            AccessibilityRotorEntry("Value: \(viewStore.state)", 0, in: namespace)
+          }
+        }
+  }
+}
+}
 #endif
