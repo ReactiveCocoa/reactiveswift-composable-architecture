6c7f9a0dfbc69afd1fec8ce604f3efc3344f9bd8
Michael Brown <me@michael-brown.net>
2022-08-06 17:36:56 +0100

    Simplify effects basics case study. (#1166)
    * Simplify effects basics case study.
    
    * wip
    
    * Update Examples/CaseStudies/SwiftUICaseStudies/02-Effects-Basics.swift
    
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>
    
    * nb
    
    Co-authored-by: Stephen Celis <stephen@stephencelis.com>

--- Examples/CaseStudies/SwiftUICaseStudiesTests/02-Effects-BasicsTests.swift
+++ Examples/CaseStudies/SwiftUICaseStudiesTests/02-Effects-BasicsTests.swift
@@ -5,7 +5,7 @@ import XCTest
 @testable import SwiftUICaseStudies
 
 class EffectsBasicsTests: XCTestCase {
-  func testCountDown() {
+  func testCountUpAndDown() {
     let store = TestStore(
       initialState: EffectsBasicsState(),
       reducer: effectsBasicsReducer,
@@ -21,20 +21,17 @@ class EffectsBasicsTests: XCTestCase {
     store.send(.decrementButtonTapped) {
       $0.count = 0
     }
-    store.receive(.incrementButtonTapped) {
-      $0.count = 1
-    }
   }
 
-  func testNumberFact() {
+  func testNumberFact_HappyPath() {
     let store = TestStore(
       initialState: EffectsBasicsState(),
       reducer: effectsBasicsReducer,
-      environment: EffectsBasicsEnvironment(
-        fact: FactClient(fetch: { n in Effect(value: "\(n) is a good number Brent") }),
-        mainQueue: ImmediateScheduler()
-      )
+      environment: .failing
     )
+
+    store.environment.fact.fetch = { Effect(value: "\($0) is a good number Brent") }
+    store.environment.mainQueue = .immediate
 
     store.send(.incrementButtonTapped) {
       $0.count = 1
@@ -47,4 +44,32 @@ class EffectsBasicsTests: XCTestCase {
       $0.numberFact = "1 is a good number Brent"
     }
   }
+
+  func testNumberFact_UnhappyPath() {
+    let store = TestStore(
+      initialState: EffectsBasicsState(),
+      reducer: effectsBasicsReducer,
+      environment: .failing
+    )
+
+    store.environment.fact.fetch = { _ in Effect(error: FactClient.Error()) }
+    store.environment.mainQueue = .immediate
+
+    store.send(.incrementButtonTapped) {
+      $0.count = 1
+    }
+    store.send(.numberFactButtonTapped) {
+      $0.isNumberFactRequestInFlight = true
+    }
+    store.receive(.numberFactResponse(.failure(FactClient.Error()))) {
+      $0.isNumberFactRequestInFlight = false
+    }
+  }
+}
+
+extension EffectsBasicsEnvironment {
+  static let failing = Self(
+    fact: .failing,
+    mainQueue: .failing
+  )
 }
