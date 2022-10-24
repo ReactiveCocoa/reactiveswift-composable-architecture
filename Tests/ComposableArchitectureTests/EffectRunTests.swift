2f784653acd3582dc35c683174ae79abb74e3d1f
stephencelis <stephencelis@users.noreply.github.com>
2022-10-24 05:37:09 +0000

    Run swift-format
    (cherry picked from commit 26da32710dfb7f3a322230d5c9bd1bf671817d9b)
    
    # Conflicts:
    #	Tests/ComposableArchitectureTests/EffectTests.swift
    #	Tests/ComposableArchitectureTests/ReducerTests.swift

--- Tests/ComposableArchitectureTests/EffectRunTests.swift
+++ Tests/ComposableArchitectureTests/EffectRunTests.swift
@@ -7,121 +7,121 @@ import XCTest
 
 // `@MainActor` introduces issues gathering tests on Linux
 #if !os(Linux)
-@MainActor
-final class EffectRunTests: XCTestCase {
-  func testRun() async {
-    struct State: Equatable {}
-    enum Action: Equatable { case tapped, response }
-    let reducer = Reduce<State, Action> { state, action in
-      switch action {
-      case .tapped:
-        return .run { send in await send(.response) }
-      case .response:
-        return .none
-      }
-    }
-    let store = TestStore(initialState: State(), reducer: reducer)
-    await store.send(.tapped)
-    await store.receive(.response)
-  }
-
-  func testRunCatch() async {
-    struct State: Equatable {}
-    enum Action: Equatable { case tapped, response }
-    let reducer = Reduce<State, Action> { state, action in
-      switch action {
-      case .tapped:
-        return .run { _ in
-          struct Failure: Error {}
-          throw Failure()
-        } catch: { @Sendable _, send in  // NB: Explicit '@Sendable' required in 5.5.2
-          await send(.response)
-        }
-      case .response:
-        return .none
-      }
-    }
-    let store = TestStore(initialState: State(), reducer: reducer)
-    await store.send(.tapped)
-    await store.receive(.response)
-  }
-
-    // `XCTExpectFailure` is not supported on Linux
-    #if DEBUG && !os(Linux)
-    func testRunUnhandledFailure() async {
-      var line: UInt!
-      XCTExpectFailure(nil, enabled: nil, strict: nil) {
-        $0.compactDescription == """
-          An "EffectTask.run" returned from "\(#fileID):\(line+1)" threw an unhandled error. …
-
-              EffectRunTests.Failure()
-
-          All non-cancellation errors must be explicitly handled via the "catch" parameter on \
-          "EffectTask.run", or via a "do" block.
-          """
-      }
+  @MainActor
+  final class EffectRunTests: XCTestCase {
+    func testRun() async {
       struct State: Equatable {}
       enum Action: Equatable { case tapped, response }
       let reducer = Reduce<State, Action> { state, action in
         switch action {
         case .tapped:
-          line = #line
-          return .run { send in
+          return .run { send in await send(.response) }
+        case .response:
+          return .none
+        }
+      }
+      let store = TestStore(initialState: State(), reducer: reducer)
+      await store.send(.tapped)
+      await store.receive(.response)
+    }
+
+    func testRunCatch() async {
+      struct State: Equatable {}
+      enum Action: Equatable { case tapped, response }
+      let reducer = Reduce<State, Action> { state, action in
+        switch action {
+        case .tapped:
+          return .run { _ in
             struct Failure: Error {}
             throw Failure()
+          } catch: { @Sendable _, send in  // NB: Explicit '@Sendable' required in 5.5.2
+            await send(.response)
           }
         case .response:
           return .none
         }
       }
       let store = TestStore(initialState: State(), reducer: reducer)
-      // NB: We wait a long time here because XCTest failures take a long time to generate
-      await store.send(.tapped).finish(timeout: 5 * NSEC_PER_SEC)
+      await store.send(.tapped)
+      await store.receive(.response)
     }
-  #endif
 
-  func testRunCancellation() async {
-    enum CancelID {}
-    struct State: Equatable {}
-    enum Action: Equatable { case tapped, response }
-    let reducer = Reduce<State, Action> { state, action in
-      switch action {
-      case .tapped:
-        return .run { send in
-          Task.cancel(id: CancelID.self)
-          try Task.checkCancellation()
-          await send(.response)
-        }
-        .cancellable(id: CancelID.self)
-      case .response:
-        return .none
-      }
-    }
-    let store = TestStore(initialState: State(), reducer: reducer)
-    await store.send(.tapped).finish()
-  }
+    // `XCTExpectFailure` is not supported on Linux
+    #if DEBUG && !os(Linux)
+      func testRunUnhandledFailure() async {
+        var line: UInt!
+        XCTExpectFailure(nil, enabled: nil, strict: nil) {
+          $0.compactDescription == """
+            An "EffectTask.run" returned from "\(#fileID):\(line+1)" threw an unhandled error. …
 
-  func testRunCancellationCatch() async {
-    enum CancelID {}
-    struct State: Equatable {}
-    enum Action: Equatable { case tapped, responseA, responseB }
-    let reducer = Reduce<State, Action> { state, action in
-      switch action {
-      case .tapped:
-        return .run { send in
-          Task.cancel(id: CancelID.self)
-          try Task.checkCancellation()
-          await send(.responseA)
-        } catch: { @Sendable _, send in  // NB: Explicit '@Sendable' required in 5.5.2
-          await send(.responseB)
+                EffectRunTests.Failure()
+
+            All non-cancellation errors must be explicitly handled via the "catch" parameter on \
+            "EffectTask.run", or via a "do" block.
+            """
         }
-        .cancellable(id: CancelID.self)
-      case .responseA, .responseB:
-        return .none
+        struct State: Equatable {}
+        enum Action: Equatable { case tapped, response }
+        let reducer = Reduce<State, Action> { state, action in
+          switch action {
+          case .tapped:
+            line = #line
+            return .run { send in
+              struct Failure: Error {}
+              throw Failure()
+            }
+          case .response:
+            return .none
+          }
+        }
+        let store = TestStore(initialState: State(), reducer: reducer)
+        // NB: We wait a long time here because XCTest failures take a long time to generate
+        await store.send(.tapped).finish(timeout: 5 * NSEC_PER_SEC)
       }
+    #endif
+
+    func testRunCancellation() async {
+      enum CancelID {}
+      struct State: Equatable {}
+      enum Action: Equatable { case tapped, response }
+      let reducer = Reduce<State, Action> { state, action in
+        switch action {
+        case .tapped:
+          return .run { send in
+            Task.cancel(id: CancelID.self)
+            try Task.checkCancellation()
+            await send(.response)
+          }
+          .cancellable(id: CancelID.self)
+        case .response:
+          return .none
+        }
+      }
+      let store = TestStore(initialState: State(), reducer: reducer)
+      await store.send(.tapped).finish()
     }
-    let store = TestStore(initialState: State(), reducer: reducer)
-    await store.send(.tapped).finish()
+
+    func testRunCancellationCatch() async {
+      enum CancelID {}
+      struct State: Equatable {}
+      enum Action: Equatable { case tapped, responseA, responseB }
+      let reducer = Reduce<State, Action> { state, action in
+        switch action {
+        case .tapped:
+          return .run { send in
+            Task.cancel(id: CancelID.self)
+            try Task.checkCancellation()
+            await send(.responseA)
+          } catch: { @Sendable _, send in  // NB: Explicit '@Sendable' required in 5.5.2
+            await send(.responseB)
+          }
+          .cancellable(id: CancelID.self)
+        case .responseA, .responseB:
+          return .none
+        }
+      }
+      let store = TestStore(initialState: State(), reducer: reducer)
+      await store.send(.tapped).finish()
+    }
   }
-}
 #endif
--- Tests/ComposableArchitectureTests/ReducerTests.swift
+++ Tests/ComposableArchitectureTests/ReducerTests.swift
@@ -5,254 +5,254 @@ import XCTest
 
 // `@MainActor` introduces issues gathering tests on Linux
 #if !os(Linux)
-@MainActor
-final class ReducerTests: XCTestCase {
-  func testCallableAsFunction() {
-    let reducer = Reduce<Int, Void> { state, _ in
-      state += 1
-      return .none
-    }
-
-    var state = 0
-    _ = reducer.reduce(into: &state, action: ())
-    XCTAssertEqual(state, 1)
-  }
-
-#if canImport(RoomPlan) || (!canImport(Darwin) && swift(>=5.7))
-  func testCombine_EffectsAreMerged() async throws {
-    if #available(iOS 16, macOS 13, tvOS 16, watchOS 9, *) {
-    enum Action: Equatable {
-      case increment
-    }
-
-    struct Delayed: ReducerProtocol {
-      typealias State = Int
-
-        @Dependency(\.continuousClock) var clock
-
-        let delay: Duration
-      let setValue: @Sendable () async -> Void
-
-      func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+  @MainActor
+  final class ReducerTests: XCTestCase {
+    func testCallableAsFunction() {
+      let reducer = Reduce<Int, Void> { state, _ in
         state += 1
-        return .fireAndForget {
-            try await self.clock.sleep(for: self.delay)
-          await self.setValue()
-        }
+        return .none
       }
+
+      var state = 0
+      _ = reducer.reduce(into: &state, action: ())
+      XCTAssertEqual(state, 1)
     }
 
-    var fastValue: Int? = nil
-    var slowValue: Int? = nil
-
-    let store = TestStore(
-      initialState: 0,
-      reducer: CombineReducers {
-          Delayed(delay: .seconds(1), setValue: { @MainActor in fastValue = 42 })
-          Delayed(delay: .seconds(2), setValue: { @MainActor in slowValue = 1729 })
-      }
-    )
-
-      let clock = TestClock()
-      store.dependencies.continuousClock = clock
-
-    await store.send(.increment) {
-      $0 = 2
-    }
-    // Waiting a second causes the fast effect to fire.
-      await clock.advance(by: .seconds(1))
-    try await Task.sleep(nanoseconds: NSEC_PER_SEC / 3)
-    XCTAssertEqual(fastValue, 42)
-    XCTAssertEqual(slowValue, nil)
-    // Waiting one more second causes the slow effect to fire. This proves that the effects
-    // are merged together, as opposed to concatenated.
-      await clock.advance(by: .seconds(1))
-    await store.finish()
-    XCTAssertEqual(fastValue, 42)
-    XCTAssertEqual(slowValue, 1729)
-  }
-  }
-  #endif
-
-  func testCombine() async {
-    enum Action: Equatable {
-      case increment
-    }
-
-    struct One: ReducerProtocol {
-      typealias State = Int
-      let effect: @Sendable () async -> Void
-      func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-        state += 1
-        return .fireAndForget {
-          await self.effect()
-        }
-      }
-    }
-
-    var first = false
-    var second = false
-
-    let store = TestStore(
-      initialState: 0,
-      reducer: CombineReducers {
-        One(effect: { @MainActor in first = true })
-        One(effect: { @MainActor in second = true })
-      }
-    )
-
-    await store
-      .send(.increment) { $0 = 2 }
-      .finish()
-
-    XCTAssertTrue(first)
-    XCTAssertTrue(second)
-  }
-
-  #if DEBUG
-    func testDebug() async {
-      enum DebugAction: Equatable {
-        case incrWithBool(Bool)
-        case incr, noop
-      }
-      struct DebugState: Equatable { var count = 0 }
-
-      var logs: [String] = []
-      let logsExpectation = self.expectation(description: "logs")
-      logsExpectation.expectedFulfillmentCount = 2
-
-      let reducer = AnyReducer<DebugState, DebugAction, Void> { state, action, _ in
-        switch action {
-        case .incrWithBool:
-          return .none
-        case .incr:
-          state.count += 1
-          return .none
-        case .noop:
-          return .none
-        }
-      }
-      .debug("[prefix]") { _ in
-        DebugEnvironment(
-          printer: {
-            logs.append($0)
-            logsExpectation.fulfill()
+    #if canImport(RoomPlan) || (!canImport(Darwin) && swift(>=5.7))
+      func testCombine_EffectsAreMerged() async throws {
+        if #available(iOS 16, macOS 13, tvOS 16, watchOS 9, *) {
+          enum Action: Equatable {
+            case increment
           }
-        )
+
+          struct Delayed: ReducerProtocol {
+            typealias State = Int
+
+            @Dependency(\.continuousClock) var clock
+
+            let delay: Duration
+            let setValue: @Sendable () async -> Void
+
+            func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+              state += 1
+              return .fireAndForget {
+                try await self.clock.sleep(for: self.delay)
+                await self.setValue()
+              }
+            }
+          }
+
+          var fastValue: Int? = nil
+          var slowValue: Int? = nil
+
+          let store = TestStore(
+            initialState: 0,
+            reducer: CombineReducers {
+              Delayed(delay: .seconds(1), setValue: { @MainActor in fastValue = 42 })
+              Delayed(delay: .seconds(2), setValue: { @MainActor in slowValue = 1729 })
+            }
+          )
+
+          let clock = TestClock()
+          store.dependencies.continuousClock = clock
+
+          await store.send(.increment) {
+            $0 = 2
+          }
+          // Waiting a second causes the fast effect to fire.
+          await clock.advance(by: .seconds(1))
+          try await Task.sleep(nanoseconds: NSEC_PER_SEC / 3)
+          XCTAssertEqual(fastValue, 42)
+          XCTAssertEqual(slowValue, nil)
+          // Waiting one more second causes the slow effect to fire. This proves that the effects
+          // are merged together, as opposed to concatenated.
+          await clock.advance(by: .seconds(1))
+          await store.finish()
+          XCTAssertEqual(fastValue, 42)
+          XCTAssertEqual(slowValue, 1729)
+        }
       }
+    #endif
+
+    func testCombine() async {
+      enum Action: Equatable {
+        case increment
+      }
+
+      struct One: ReducerProtocol {
+        typealias State = Int
+        let effect: @Sendable () async -> Void
+        func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+          state += 1
+          return .fireAndForget {
+            await self.effect()
+          }
+        }
+      }
+
+      var first = false
+      var second = false
 
       let store = TestStore(
-        initialState: .init(),
-        reducer: reducer,
-        environment: ()
+        initialState: 0,
+        reducer: CombineReducers {
+          One(effect: { @MainActor in first = true })
+          One(effect: { @MainActor in second = true })
+        }
       )
-      await store.send(.incr) { $0.count = 1 }
-      await store.send(.noop)
 
-      self.wait(for: [logsExpectation], timeout: 5)
+      await store
+        .send(.increment) { $0 = 2 }
+        .finish()
 
-      XCTAssertEqual(
-        logs,
-        [
-          #"""
-          [prefix]: received action:
-            ReducerTests.DebugAction.incr
-          - ReducerTests.DebugState(count: 0)
-          + ReducerTests.DebugState(count: 1)
-
-          """#,
-          #"""
-          [prefix]: received action:
-            ReducerTests.DebugAction.noop
-            (No state changes)
-
-          """#,
-        ]
-      )
+      XCTAssertTrue(first)
+      XCTAssertTrue(second)
     }
 
-    func testDebug_ActionFormat_OnlyLabels() {
-      enum DebugAction: Equatable {
-        case incrWithBool(Bool)
-        case incr, noop
-      }
-      struct DebugState: Equatable { var count = 0 }
-
-      var logs: [String] = []
-      let logsExpectation = self.expectation(description: "logs")
-
-      let reducer = AnyReducer<DebugState, DebugAction, Void> { state, action, _ in
-        switch action {
-        case let .incrWithBool(bool):
-          state.count += bool ? 1 : 0
-          return .none
-        default:
-          return .none
+    #if DEBUG
+      func testDebug() async {
+        enum DebugAction: Equatable {
+          case incrWithBool(Bool)
+          case incr, noop
         }
-      }
-      .debug("[prefix]", actionFormat: .labelsOnly) { _ in
-        DebugEnvironment(
-          printer: {
-            logs.append($0)
-            logsExpectation.fulfill()
-          }
-        )
-      }
+        struct DebugState: Equatable { var count = 0 }
 
-      let viewStore = ViewStore(
-        Store(
+        var logs: [String] = []
+        let logsExpectation = self.expectation(description: "logs")
+        logsExpectation.expectedFulfillmentCount = 2
+
+        let reducer = AnyReducer<DebugState, DebugAction, Void> { state, action, _ in
+          switch action {
+          case .incrWithBool:
+            return .none
+          case .incr:
+            state.count += 1
+            return .none
+          case .noop:
+            return .none
+          }
+        }
+        .debug("[prefix]") { _ in
+          DebugEnvironment(
+            printer: {
+              logs.append($0)
+              logsExpectation.fulfill()
+            }
+          )
+        }
+
+        let store = TestStore(
           initialState: .init(),
           reducer: reducer,
           environment: ()
         )
-      )
-      viewStore.send(.incrWithBool(true))
+        await store.send(.incr) { $0.count = 1 }
+        await store.send(.noop)
 
-      self.wait(for: [logsExpectation], timeout: 5)
+        self.wait(for: [logsExpectation], timeout: 5)
 
-      XCTAssertEqual(
-        logs,
-        [
-          #"""
-          [prefix]: received action:
-            ReducerTests.DebugAction.incrWithBool
-          - ReducerTests.DebugState(count: 0)
-          + ReducerTests.DebugState(count: 1)
+        XCTAssertEqual(
+          logs,
+          [
+            #"""
+            [prefix]: received action:
+              ReducerTests.DebugAction.incr
+            - ReducerTests.DebugState(count: 0)
+            + ReducerTests.DebugState(count: 1)
 
-          """#
-        ]
-      )
-    }
-  #endif
+            """#,
+            #"""
+            [prefix]: received action:
+              ReducerTests.DebugAction.noop
+              (No state changes)
+
+            """#,
+          ]
+        )
+      }
+
+      func testDebug_ActionFormat_OnlyLabels() {
+        enum DebugAction: Equatable {
+          case incrWithBool(Bool)
+          case incr, noop
+        }
+        struct DebugState: Equatable { var count = 0 }
+
+        var logs: [String] = []
+        let logsExpectation = self.expectation(description: "logs")
+
+        let reducer = AnyReducer<DebugState, DebugAction, Void> { state, action, _ in
+          switch action {
+          case let .incrWithBool(bool):
+            state.count += bool ? 1 : 0
+            return .none
+          default:
+            return .none
+          }
+        }
+        .debug("[prefix]", actionFormat: .labelsOnly) { _ in
+          DebugEnvironment(
+            printer: {
+              logs.append($0)
+              logsExpectation.fulfill()
+            }
+          )
+        }
+
+        let viewStore = ViewStore(
+          Store(
+            initialState: .init(),
+            reducer: reducer,
+            environment: ()
+          )
+        )
+        viewStore.send(.incrWithBool(true))
+
+        self.wait(for: [logsExpectation], timeout: 5)
+
+        XCTAssertEqual(
+          logs,
+          [
+            #"""
+            [prefix]: received action:
+              ReducerTests.DebugAction.incrWithBool
+            - ReducerTests.DebugState(count: 0)
+            + ReducerTests.DebugState(count: 1)
+
+            """#
+          ]
+        )
+      }
+    #endif
 
     #if canImport(os)
       @available(iOS 12.0, *)
-  func testDefaultSignpost() {
-    let reducer = EmptyReducer<Int, Void>().signpost(log: .default)
-    var n = 0
-    let effect = reducer.reduce(into: &n, action: ())
-    let expectation = self.expectation(description: "effect")
-    effect
+      func testDefaultSignpost() {
+        let reducer = EmptyReducer<Int, Void>().signpost(log: .default)
+        var n = 0
+        let effect = reducer.reduce(into: &n, action: ())
+        let expectation = self.expectation(description: "effect")
+        effect
           .producer
           .startWithCompleted {
             expectation.fulfill()
           }
-    self.wait(for: [expectation], timeout: 0.1)
-  }
+        self.wait(for: [expectation], timeout: 0.1)
+      }
 
       @available(iOS 12.0, *)
-  func testDisabledSignpost() {
-    let reducer = EmptyReducer<Int, Void>().signpost(log: .disabled)
-    var n = 0
-    let effect = reducer.reduce(into: &n, action: ())
-    let expectation = self.expectation(description: "effect")
-    effect
+      func testDisabledSignpost() {
+        let reducer = EmptyReducer<Int, Void>().signpost(log: .disabled)
+        var n = 0
+        let effect = reducer.reduce(into: &n, action: ())
+        let expectation = self.expectation(description: "effect")
+        effect
           .producer
           .startWithCompleted {
             expectation.fulfill()
           }
-    self.wait(for: [expectation], timeout: 0.1)
-  }
+        self.wait(for: [expectation], timeout: 0.1)
+      }
     #endif
-}
+  }
 #endif
