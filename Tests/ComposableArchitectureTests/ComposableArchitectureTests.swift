38a941132cef5b2ad83506feaf886b769d25a020
Stephen Celis <stephen@stephencelis.com>
2022-10-28 10:54:36 -0700

    Use opaque reducers in 5.7 to fix builder inference (#1591)
    * Use opaque reducers in 5.7 to fix builder inference
    
    * wip
    
    * wip
    
    * Revert "wip"
    
    This reverts commit f51e5690f62d10df5af37e85d2509a311fb5916e.
    
    * wip
    
    * Fix availability; simplify optionality
    
    (cherry picked from commit 9413d9fbcd057f353891c9f3b791cac7c41f59ee)
    
    # Conflicts:
    #	Tests/ComposableArchitectureTests/CompatibilityTests.swift
    #	Tests/ComposableArchitectureTests/ComposableArchitectureTests.swift

--- Tests/ComposableArchitectureTests/ComposableArchitectureTests.swift
+++ Tests/ComposableArchitectureTests/ComposableArchitectureTests.swift
@@ -4,155 +4,155 @@ import XCTest
 
 // `@MainActor` introduces issues gathering tests on Linux
 #if !os(Linux)
-  @MainActor
-  final class ComposableArchitectureTests: XCTestCase {
-    func testScheduling() async {
-      struct Counter: ReducerProtocol {
-        typealias State = Int
-        enum Action: Equatable {
-          case incrAndSquareLater
-          case incrNow
-          case squareNow
-        }
-        @Dependency(\.mainQueue) var mainQueue
-        func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-          switch action {
-          case .incrAndSquareLater:
-            return .merge(
-              EffectTask(value: .incrNow)
+@MainActor
+final class ComposableArchitectureTests: XCTestCase {
+  func testScheduling() async {
+    struct Counter: ReducerProtocol {
+      typealias State = Int
+      enum Action: Equatable {
+        case incrAndSquareLater
+        case incrNow
+        case squareNow
+      }
+      @Dependency(\.mainQueue) var mainQueue
+      func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .incrAndSquareLater:
+          return .merge(
+            EffectTask(value: .incrNow)
                 .deferred(for: 2, scheduler: self.mainQueue),
-              EffectTask(value: .squareNow)
+            EffectTask(value: .squareNow)
                 .deferred(for: 1, scheduler: self.mainQueue),
-              EffectTask(value: .squareNow)
+            EffectTask(value: .squareNow)
                 .deferred(for: 2, scheduler: self.mainQueue)
-            )
-          case .incrNow:
-            state += 1
-            return .none
-          case .squareNow:
-            state *= state
-            return .none
-          }
+          )
+        case .incrNow:
+          state += 1
+          return .none
+        case .squareNow:
+          state *= state
+          return .none
         }
       }
+    }
 
-      let store = TestStore(
-        initialState: 2,
-        reducer: Counter()
-      )
+    let store = TestStore(
+      initialState: 2,
+      reducer: Counter()
+    )
 
       let mainQueue = TestScheduler()
       store.dependencies.mainQueue = mainQueue
 
-      await store.send(.incrAndSquareLater)
-      await mainQueue.advance(by: 1)
-      await store.receive(.squareNow) { $0 = 4 }
-      await mainQueue.advance(by: 1)
-      await store.receive(.incrNow) { $0 = 5 }
-      await store.receive(.squareNow) { $0 = 25 }
+    await store.send(.incrAndSquareLater)
+    await mainQueue.advance(by: 1)
+    await store.receive(.squareNow) { $0 = 4 }
+    await mainQueue.advance(by: 1)
+    await store.receive(.incrNow) { $0 = 5 }
+    await store.receive(.squareNow) { $0 = 25 }
 
-      await store.send(.incrAndSquareLater)
-      await mainQueue.advance(by: 2)
-      await store.receive(.squareNow) { $0 = 625 }
-      await store.receive(.incrNow) { $0 = 626 }
-      await store.receive(.squareNow) { $0 = 391876 }
-    }
+    await store.send(.incrAndSquareLater)
+    await mainQueue.advance(by: 2)
+    await store.receive(.squareNow) { $0 = 625 }
+    await store.receive(.incrNow) { $0 = 626 }
+    await store.receive(.squareNow) { $0 = 391876 }
+  }
 
-    func testSimultaneousWorkOrdering() {
+  func testSimultaneousWorkOrdering() {
       let mainQueue = TestScheduler()
 
-      var values: [Int] = []
+    var values: [Int] = []
       mainQueue.schedule(after: .seconds(0), interval: .seconds(1)) { values.append(1) }
       mainQueue.schedule(after: .seconds(0), interval: .seconds(2)) { values.append(42) }
 
-      XCTAssertEqual(values, [])
-      mainQueue.advance()
-      XCTAssertEqual(values, [1, 42])
-      mainQueue.advance(by: 2)
+    XCTAssertEqual(values, [])
+    mainQueue.advance()
+    XCTAssertEqual(values, [1, 42])
+    mainQueue.advance(by: 2)
       XCTAssertEqual(values, [1, 42, 1, 42, 1])
-    }
+  }
 
-    func testLongLivingEffects() async {
-      typealias Environment = (
-        startEffect: EffectTask<Void>,
-        stopEffect: EffectTask<Never>
-      )
+  func testLongLivingEffects() async {
+    typealias Environment = (
+      startEffect: EffectTask<Void>,
+      stopEffect: EffectTask<Never>
+    )
 
-      enum Action { case end, incr, start }
+    enum Action { case end, incr, start }
 
-      let effect = AsyncStream<Void>.streamWithContinuation()
+    let effect = AsyncStream<Void>.streamWithContinuation()
 
-      let reducer = Reduce<Int, Action> { state, action in
-        switch action {
-        case .end:
-          return .fireAndForget {
-            effect.continuation.finish()
-          }
-        case .incr:
-          state += 1
-          return .none
-        case .start:
-          return .run { send in
-            for await _ in effect.stream {
-              await send(.incr)
-            }
+    let reducer = Reduce<Int, Action> { state, action in
+      switch action {
+      case .end:
+        return .fireAndForget {
+          effect.continuation.finish()
+        }
+      case .incr:
+        state += 1
+        return .none
+      case .start:
+        return .run { send in
+          for await _ in effect.stream {
+            await send(.incr)
           }
         }
       }
-
-      let store = TestStore(initialState: 0, reducer: reducer)
-
-      await store.send(.start)
-      await store.send(.incr) { $0 = 1 }
-      effect.continuation.yield()
-      await store.receive(.incr) { $0 = 2 }
-      await store.send(.end)
     }
 
-    func testCancellation() async {
+    let store = TestStore(initialState: 0, reducer: reducer)
+
+    await store.send(.start)
+    await store.send(.incr) { $0 = 1 }
+    effect.continuation.yield()
+    await store.receive(.incr) { $0 = 2 }
+    await store.send(.end)
+  }
+
+  func testCancellation() async {
       let mainQueue = TestScheduler()
 
-      enum Action: Equatable {
-        case cancel
-        case incr
-        case response(Int)
-      }
+    enum Action: Equatable {
+      case cancel
+      case incr
+      case response(Int)
+    }
 
-      let reducer = Reducer<Int, Action, Void> { state, action, _ in
-        enum CancelID {}
+    let reducer = AnyReducer<Int, Action, Void> { state, action, _ in
+      enum CancelID {}
 
-        switch action {
-        case .cancel:
-          return .cancel(id: CancelID.self)
+      switch action {
+      case .cancel:
+        return .cancel(id: CancelID.self)
 
-        case .incr:
-          state += 1
-          return .task { [state] in
-            try await mainQueue.sleep(for: .seconds(1))
-            return .response(state * state)
-          }
-          .cancellable(id: CancelID.self)
-
-        case let .response(value):
-          state = value
-          return .none
+      case .incr:
+        state += 1
+        return .task { [state] in
+          try await mainQueue.sleep(for: .seconds(1))
+          return .response(state * state)
         }
+        .cancellable(id: CancelID.self)
+
+      case let .response(value):
+        state = value
+        return .none
       }
+    }
 
-      let store = TestStore(
-        initialState: 0,
-        reducer: reducer,
-        environment: ()
-      )
+    let store = TestStore(
+      initialState: 0,
+      reducer: reducer,
+      environment: ()
+    )
 
-      await store.send(.incr) { $0 = 1 }
-      await mainQueue.advance(by: .seconds(1))
-      await store.receive(.response(1))
+    await store.send(.incr) { $0 = 1 }
+    await mainQueue.advance(by: .seconds(1))
+    await store.receive(.response(1))
 
-      await store.send(.incr) { $0 = 2 }
-      await store.send(.cancel)
+    await store.send(.incr) { $0 = 2 }
+    await store.send(.cancel)
       // NB: Wait a bit more time to handle effects so this test is less brittle in CI
       await store.finish(timeout: NSEC_PER_SEC)
-    }
   }
+}
 #endif
