pointfreeco/swift-composable-architecture

Simplify re-entrant tests (#1370)
* wip

* wip

* wip

Tests/ComposableArchitectureTests/CompatibilityTests.swift
@@ -5,7 +5,10 @@ import XCTest
 
 @MainActor
 final class CompatibilityTests: XCTestCase {
-  func testCaseStudy_ReentrantActionsFromBuffer() {
+  // Actions can be re-entrantly sent into the store if an action is sent that holds an object
+  // which sends an action on deinit. In order to prevent a simultaneous access exception for this
+  // case we need to use `withExtendedLifetime` on the buffered actions when clearing them out.
+  func testCaseStudy_ActionReentranceFromClearedBufferCausingDeinitAction() {
     let cancelID = UUID()
 
     struct State: Equatable {}
@@ -75,55 +78,40 @@ final class CompatibilityTests: XCTestCase {
     )
   }
 
-  func testCaseStudy_ReentrantActionsFromPublisher() {
-    struct State: Equatable {
-      var city: String
-      var country: String
-    }
-
-    enum Action: Equatable {
-      case updateCity(String)
-      case updateCountry(String)
-    }
-
-    let reducer = Reducer<State, Action, Void> { state, action, _ in
-      switch action {
-      case let .updateCity(city):
-        state.city = city
+  // Actions can be re-entrantly sent into the store while observing changes to the store's state.
+  // In such cases we need to take special care that those re-entrant actions are handled _after_
+  // the original action.
+  //
+  // In particular, this means that in the implementation of `Store.send` we need to flip
+  // `isSending` to false _after_ the store's state mutation is made so that re-entrant actions
+  // are buffered rather than immediately handled.
+  func testCaseStudy_ActionReentranceFromStateObservation() {
+    let store = Store<Int, Int>(
+      initialState: 0,
+      reducer: .init { state, action, _ in
+        state = action
         return .none
-      case let .updateCountry(country):
-        state.country = country
-        return .none
-      }
-    }
-
-    let store = Store(
-      initialState: State(city: "New York", country: "USA"),
-      reducer: reducer,
+      },
       environment: ()
     )
+
     let viewStore = ViewStore(store)
 
     var cancellables: Set<AnyCancellable> = []
-
-    viewStore.publisher.city
-      .sink { city in
-        if city == "London" {
-          viewStore.send(.updateCountry("UK"))
-        }
+    viewStore.publisher
+      .sink { value in
+        if value == 1 { viewStore.send(0) }
       }
       .store(in: &cancellables)
 
-    var countryUpdates = [String]()
-    viewStore.publisher.country
-      .sink { country in
-        countryUpdates.append(country)
-      }
+    var stateChanges: [Int] = []
+    viewStore.publisher
+      .sink { stateChanges.append($0) }
       .store(in: &cancellables)
 
-    viewStore.send(.updateCity("London"))
-
-    XCTAssertEqual(countryUpdates, ["USA", "UK"])
+    XCTAssertEqual(stateChanges, [0])
+    viewStore.send(1)
+    XCTAssertEqual(stateChanges, [0, 1, 0])
   }
 }
 

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit 7940588a9e413004a6efd4c34a2d7f34a8852420)
# Conflicts:
#	Sources/ComposableArchitecture/Store.swift
#	Tests/ComposableArchitectureTests/CompatibilityTests.swift

Tests/ComposableArchitectureTests/CompatibilityTests.swift
@@ -4,7 +4,10 @@ import XCTest
 
 @MainActor
 final class CompatibilityTests: XCTestCase {
-  func testCaseStudy_ReentrantActionsFromBuffer() {
+  // Actions can be re-entrantly sent into the store if an action is sent that holds an object
+  // which sends an action on deinit. In order to prevent a simultaneous access exception for this
+  // case we need to use `withExtendedLifetime` on the buffered actions when clearing them out.
+  func testCaseStudy_ActionReentranceFromClearedBufferCausingDeinitAction() {
     let cancelID = UUID()
 
     struct State: Equatable {}
@@ -73,51 +76,37 @@ final class CompatibilityTests: XCTestCase {
     )
   }
 
-  func testCaseStudy_ReentrantActionsFromProducer() {
-    struct State: Equatable {
-      var city: String
-      var country: String
-    }
-
-    enum Action: Equatable {
-      case updateCity(String)
-      case updateCountry(String)
-    }
-
-    let reducer = Reducer<State, Action, Void> { state, action, _ in
-      switch action {
-      case let .updateCity(city):
-        state.city = city
+  // Actions can be re-entrantly sent into the store while observing changes to the store's state.
+  // In such cases we need to take special care that those re-entrant actions are handled _after_
+  // the original action.
+  //
+  // In particular, this means that in the implementation of `Store.send` we need to flip
+  // `isSending` to false _after_ the store's state mutation is made so that re-entrant actions
+  // are buffered rather than immediately handled.
+  func testCaseStudy_ActionReentranceFromStateObservation() {
+    let store = Store<Int, Int>(
+      initialState: 0,
+      reducer: .init { state, action, _ in
+        state = action
         return .none
-      case let .updateCountry(country):
-        state.country = country
-        return .none
-      }
-    }
-
-    let store = Store(
-      initialState: State(city: "New York", country: "USA"),
-      reducer: reducer,
+      },
       environment: ()
     )
+
     let viewStore = ViewStore(store)
 
-    viewStore.produced.city
-      .startWithValues { city in
-        if city == "London" {
-          viewStore.send(.updateCountry("UK"))
-        }
+    viewStore.produced.producer
+      .startWithValues { value in
+        if value == 1 { viewStore.send(0) }
       }
 
-    var countryUpdates = [String]()
-    viewStore.produced.country
-      .startWithValues { country in
-        countryUpdates.append(country)
-      }
+    var stateChanges: [Int] = []
+    viewStore.produced.producer
+      .startWithValues { stateChanges.append($0) }
 
-    viewStore.send(.updateCity("London"))
-
-    XCTAssertEqual(countryUpdates, ["USA", "UK"])
+    XCTAssertEqual(stateChanges, [0])
+    viewStore.send(1)
+    XCTAssertEqual(stateChanges, [0, 1, 0])
   }
 }
 