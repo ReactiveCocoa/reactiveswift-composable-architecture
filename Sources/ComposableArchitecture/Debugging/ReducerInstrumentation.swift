pointfreeco/swift-composable-architecture

Update Effect internals to be more efficient. (#1312)
* Distinguish Combine and async effects from each other

* wip

* wip

* wip

* Add some tests

* wip

* wip

* wip

Co-authored-by: Stephen Celis <stephen@stephencelis.com>

Sources/ComposableArchitecture/Debugging/ReducerInstrumentation.swift
@@ -47,43 +47,71 @@ extension Reducer {
         return
           effects
           .effectSignpost(prefix, log: log, actionOutput: actionOutput)
-          .eraseToEffect()
       }
       return effects
     }
   }
 }
 
-extension Publisher where Failure == Never {
+extension Effect where Failure == Never {
   func effectSignpost(
     _ prefix: String,
     log: OSLog,
     actionOutput: String
-  ) -> Publishers.HandleEvents<Self> {
+  ) -> Self {
     let sid = OSSignpostID(log: log)
 
-    return
-      self
-      .handleEvents(
-        receiveSubscription: { _ in
+    switch self.operation {
+    case .none:
+      return self
+    case let .publisher(publisher):
+      return .init(
+        operation: .publisher(
+          publisher.handleEvents(
+            receiveSubscription: { _ in
+              os_signpost(
+                .begin, log: log, name: "Effect", signpostID: sid, "%sStarted from %s", prefix,
+                actionOutput)
+            },
+            receiveOutput: { value in
+              os_signpost(
+                .event, log: log, name: "Effect Output", "%sOutput from %s", prefix, actionOutput)
+            },
+            receiveCompletion: { completion in
+              switch completion {
+              case .finished:
+                os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sFinished", prefix)
+              }
+            },
+            receiveCancel: {
+              os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sCancelled", prefix)
+            }
+          )
+          .eraseToAnyPublisher()
+        )
+      )
+    case let .run(priority, operation):
+      return .init(
+        operation: .run(priority) { send in
           os_signpost(
             .begin, log: log, name: "Effect", signpostID: sid, "%sStarted from %s", prefix,
-            actionOutput)
-        },
-        receiveOutput: { value in
-          os_signpost(
-            .event, log: log, name: "Effect Output", "%sOutput from %s", prefix, actionOutput)
-        },
-        receiveCompletion: { completion in
-          switch completion {
-          case .finished:
-            os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sFinished", prefix)
+            actionOutput
+          )
+          await operation(
+            Send { output in
+              os_signpost(
+                .event, log: log, name: "Effect Output", "%sOutput from %s", prefix, actionOutput
+              )
+              send(output)
+            }
+          )
+          if Task.isCancelled {
+            os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sCancelled", prefix)
           }
-        },
-        receiveCancel: {
-          os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sCancelled", prefix)
+          os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sFinished", prefix)
         }
       )
+    }
   }
 }
 

Sources/ComposableArchitecture/Effects/Debouncing.swift
@@ -30,12 +30,32 @@ extension Effect {
     scheduler: S,
     options: S.SchedulerOptions? = nil
   ) -> Self {
-    Just(())
-      .setFailureType(to: Failure.self)
-      .delay(for: dueTime, scheduler: scheduler, options: options)
-      .flatMap { self.receive(on: scheduler) }
-      .eraseToEffect()
+    switch self.operation {
+    case .none:
+      return .none
+    case let .publisher(publisher):
+      return Self(
+        operation: .publisher(
+          Just(())
+            .setFailureType(to: Failure.self)
+            .delay(for: dueTime, scheduler: scheduler, options: options)
+            .flatMap { publisher.receive(on: scheduler) }
+            .eraseToAnyPublisher()
+        )
+      )
       .cancellable(id: id, cancelInFlight: true)
+    case let .run(priority, operation):
+      return Self(
+        operation: .run(priority) { send in
+          await withTaskCancellation(id: id, cancelInFlight: true) {
+            do {
+              try await scheduler.sleep(for: dueTime, options: options)
+              await operation(send)
+            } catch {}
+          }
+        }
+      )
+    }
   }
 
   /// Turns an effect into one that can be debounced.

Sources/ComposableArchitecture/Effects/Throttling.swift
@@ -20,44 +20,49 @@ extension Effect {
     scheduler: S,
     latest: Bool
   ) -> Self {
-    self.receive(on: scheduler)
-      .flatMap { value -> AnyPublisher<Output, Failure> in
-        throttleLock.lock()
-        defer { throttleLock.unlock() }
+    switch self.operation {
+    case .none:
+      return .none
+    case .publisher, .run:
+      return self.receive(on: scheduler)
+        .flatMap { value -> AnyPublisher<Output, Failure> in
+          throttleLock.lock()
+          defer { throttleLock.unlock() }
 
-        guard let throttleTime = throttleTimes[id] as! S.SchedulerTimeType? else {
-          throttleTimes[id] = scheduler.now
-          throttleValues[id] = nil
-          return Just(value).setFailureType(to: Failure.self).eraseToAnyPublisher()
-        }
+          guard let throttleTime = throttleTimes[id] as! S.SchedulerTimeType? else {
+            throttleTimes[id] = scheduler.now
+            throttleValues[id] = nil
+            return Just(value).setFailureType(to: Failure.self).eraseToAnyPublisher()
+          }
 
-        let value = latest ? value : (throttleValues[id] as! Output? ?? value)
-        throttleValues[id] = value
+          let value = latest ? value : (throttleValues[id] as! Output? ?? value)
+          throttleValues[id] = value
 
-        guard throttleTime.distance(to: scheduler.now) < interval else {
-          throttleTimes[id] = scheduler.now
-          throttleValues[id] = nil
-          return Just(value).setFailureType(to: Failure.self).eraseToAnyPublisher()
-        }
+          guard throttleTime.distance(to: scheduler.now) < interval else {
+            throttleTimes[id] = scheduler.now
+            throttleValues[id] = nil
+            return Just(value).setFailureType(to: Failure.self).eraseToAnyPublisher()
+          }
 
-        return Just(value)
-          .delay(
-            for: scheduler.now.distance(to: throttleTime.advanced(by: interval)),
-            scheduler: scheduler
-          )
-          .handleEvents(
-            receiveOutput: { _ in
-              throttleLock.sync {
-                throttleTimes[id] = scheduler.now
-                throttleValues[id] = nil
+          return Just(value)
+            .delay(
+              for: scheduler.now.distance(to: throttleTime.advanced(by: interval)),
+              scheduler: scheduler
+            )
+            .handleEvents(
+              receiveOutput: { _ in
+                throttleLock.sync {
+                  throttleTimes[id] = scheduler.now
+                  throttleValues[id] = nil
+                }
               }
-            }
-          )
-          .setFailureType(to: Failure.self)
-          .eraseToAnyPublisher()
-      }
-      .eraseToEffect()
-      .cancellable(id: id, cancelInFlight: true)
+            )
+            .setFailureType(to: Failure.self)
+            .eraseToAnyPublisher()
+        }
+        .eraseToEffect()
+        .cancellable(id: id, cancelInFlight: true)
+    }
   }
 
   /// Throttles an effect so that it only publishes one output per given interval.

Sources/ComposableArchitecture/Store.swift
@@ -357,40 +357,57 @@ public final class Store<State, Action> {
       let action = self.bufferedActions.removeFirst()
       let effect = self.reducer(&currentState, action)
 
-      var didComplete = false
-      let boxedTask = Box<Task<Void, Never>?>(wrappedValue: nil)
-      let uuid = UUID()
-      let effectCancellable =
-        effect
-        .handleEvents(
-          receiveCancel: { [weak self] in
-            self?.threadCheck(status: .effectCompletion(action))
-            self?.effectCancellables[uuid] = nil
-          }
-        )
-        .sink(
-          receiveCompletion: { [weak self] _ in
-            self?.threadCheck(status: .effectCompletion(action))
-            boxedTask.wrappedValue?.cancel()
-            didComplete = true
-            self?.effectCancellables[uuid] = nil
-          },
-          receiveValue: { [weak self] effectAction in
-            guard let self = self else { return }
-            if let task = self.send(effectAction, originatingFrom: action) {
-              tasks.wrappedValue.append(task)
+      switch effect.operation {
+      case .none:
+        break
+      case let .publisher(publisher):
+        var didComplete = false
+        let boxedTask = Box<Task<Void, Never>?>(wrappedValue: nil)
+        let uuid = UUID()
+        let effectCancellable =
+          publisher
+          .handleEvents(
+            receiveCancel: { [weak self] in
+              self?.threadCheck(status: .effectCompletion(action))
+              self?.effectCancellables[uuid] = nil
             }
+          )
+          .sink(
+            receiveCompletion: { [weak self] _ in
+              self?.threadCheck(status: .effectCompletion(action))
+              boxedTask.wrappedValue?.cancel()
+              didComplete = true
+              self?.effectCancellables[uuid] = nil
+            },
+            receiveValue: { [weak self] effectAction in
+              guard let self = self else { return }
+              if let task = self.send(effectAction, originatingFrom: action) {
+                tasks.wrappedValue.append(task)
+              }
+            }
+          )
+
+        if !didComplete {
+          let task = Task<Void, Never> { @MainActor in
+            for await _ in AsyncStream<Void>.never {}
+            effectCancellable.cancel()
+          }
+          boxedTask.wrappedValue = task
+          tasks.wrappedValue.append(task)
+          self.effectCancellables[uuid] = effectCancellable
+        }
+      case let .run(priority, operation):
+        tasks.wrappedValue.append(
+          Task(priority: priority) {
+            await operation(
+              Send {
+                if let task = self.send($0, originatingFrom: action) {
+                  tasks.wrappedValue.append(task)
+                }
+              }
+            )
           }
         )
-
-      if !didComplete {
-        let task = Task<Void, Never> { @MainActor in
-          for await _ in AsyncStream<Void>.never {}
-          effectCancellable.cancel()
-        }
-        boxedTask.wrappedValue = task
-        tasks.wrappedValue.append(task)
-        self.effectCancellables[uuid] = effectCancellable
       }
     }
 

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit 7b5bc85f2eac9e8290b74e41f5a8b3d1136b1928)
# Conflicts:
#	Sources/ComposableArchitecture/Debugging/ReducerDebugging.swift
#	Sources/ComposableArchitecture/Debugging/ReducerInstrumentation.swift
#	Sources/ComposableArchitecture/Effect.swift
#	Sources/ComposableArchitecture/Effects/Animation.swift
#	Sources/ComposableArchitecture/Effects/Cancellation.swift
#	Sources/ComposableArchitecture/Effects/Debouncing.swift
#	Sources/ComposableArchitecture/Effects/Deferring.swift
#	Sources/ComposableArchitecture/Effects/SignalProducer.swift
#	Sources/ComposableArchitecture/Effects/Throttling.swift
#	Sources/ComposableArchitecture/Internal/TaskCancellableValue.swift
#	Sources/ComposableArchitecture/Store.swift
#	Tests/ComposableArchitectureTests/EffectFailureTests.swift
#	Tests/ComposableArchitectureTests/EffectTests.swift

Sources/ComposableArchitecture/Debugging/ReducerInstrumentation.swift
@@ -58,30 +58,59 @@
       _ prefix: String,
       log: OSLog,
       actionOutput: String
-    ) -> Effect<Output, Failure> {
+    ) -> Self {
       let sid = OSSignpostID(log: log)
 
-      return self.producer
-        .on(
-          starting: {
+      switch self.operation {
+      case .none:
+        return self
+      case let .producer(producer):
+        return .init(
+          operation: .producer(
+            producer
+              .on(
+                starting: {
+                  os_signpost(
+                    .begin, log: log, name: "Effect", signpostID: sid, "%sStarted from %s", prefix,
+                    actionOutput
+                  )
+                },
+                completed: {
+                  os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sFinished", prefix)
+                },
+                disposed: {
+                  os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sCancelled", prefix)
+                },
+                value: { value in
+                  os_signpost(
+                    .event, log: log, name: "Effect Output", "%sOutput from %s", prefix, actionOutput
+                  )
+                }
+              )
+          )
+        )
+      case let .run(priority, operation):
+        return .init(
+          operation: .run(priority) { send in
             os_signpost(
               .begin, log: log, name: "Effect", signpostID: sid, "%sStarted from %s", prefix,
               actionOutput
             )
-          },
-          completed: {
-            os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sFinished", prefix)
-          },
-          disposed: {
-            os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sCancelled", prefix)
-          },
-          value: { value in
-            os_signpost(
-              .event, log: log, name: "Effect Output", "%sOutput from %s", prefix, actionOutput
+            await operation(
+              Send { output in
+                os_signpost(
+                  .event, log: log, name: "Effect Output", "%sOutput from %s", prefix, actionOutput
+                )
+                send(output)
+              }
             )
+            if Task.isCancelled {
+              os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sCancelled", prefix)
+            }
+            os_signpost(.end, log: log, name: "Effect", signpostID: sid, "%sFinished", prefix)
           }
         )
-        .eraseToEffect()
+      }
     }
   }
 #endif

Sources/ComposableArchitecture/Effects/Debouncing.swift
@@ -29,12 +29,31 @@ extension Effect {
     for dueTime: TimeInterval,
     scheduler: DateScheduler
   ) -> Self {
-    SignalProducer<Void, Never>.init(value: ())
-      .promoteError(Failure.self)
-      .delay(dueTime, on: scheduler)
-      .flatMap(.latest) { self.producer.observe(on: scheduler) }
-      .eraseToEffect()
+    switch self.operation {
+    case .none:
+      return .none
+    case let .producer(producer):
+      return Self(
+        operation: .producer(
+          SignalProducer<Void, Never>.init(value: ())
+            .promoteError(Failure.self)
+            .delay(dueTime, on: scheduler)
+            .flatMap(.latest) { producer.observe(on: scheduler) }
+        )
+      )
       .cancellable(id: id, cancelInFlight: true)
+    case let .run(priority, operation):
+      return Self(
+        operation: .run(priority) { send in
+          await withTaskCancellation(id: id, cancelInFlight: true) {
+            do {
+              try await scheduler.sleep(for: .nanoseconds(Int(dueTime * TimeInterval(NSEC_PER_SEC))))
+              await operation(send)
+            } catch {}
+          }
+        }
+      )
+    }
   }
 
   /// Turns an effect into one that can be debounced.

Sources/ComposableArchitecture/Effects/Throttling.swift
@@ -20,46 +20,51 @@ extension Effect {
     scheduler: DateScheduler,
     latest: Bool
   ) -> Self {
-    self.producer
-      .observe(on: scheduler)
-      .flatMap(.latest) { value -> SignalProducer<Output, Failure> in
-        throttleLock.lock()
-        defer { throttleLock.unlock() }
+    switch self.operation {
+    case .none:
+      return .none
+    case .producer, .run:
+      return self.producer
+        .observe(on: scheduler)
+        .flatMap(.latest) { value -> SignalProducer<Output, Failure> in
+          throttleLock.lock()
+          defer { throttleLock.unlock() }
 
-        guard let throttleTime = throttleTimes[id] as! Date? else {
-          throttleTimes[id] = scheduler.currentDate
-          throttleValues[id] = nil
+          guard let throttleTime = throttleTimes[id] as! Date? else {
+            throttleTimes[id] = scheduler.currentDate
+            throttleValues[id] = nil
+            return SignalProducer(value: value)
+          }
+
+          let value = latest ? value : (throttleValues[id] as! Output? ?? value)
+          throttleValues[id] = value
+
+          guard
+            scheduler.currentDate.timeIntervalSince1970 - throttleTime.timeIntervalSince1970
+              < interval
+          else {
+            throttleTimes[id] = scheduler.currentDate
+            throttleValues[id] = nil
+            return SignalProducer(value: value)
+          }
+
           return SignalProducer(value: value)
-        }
-
-        let value = latest ? value : (throttleValues[id] as! Output? ?? value)
-        throttleValues[id] = value
-
-        guard
-          scheduler.currentDate.timeIntervalSince1970 - throttleTime.timeIntervalSince1970
-            < interval
-        else {
-          throttleTimes[id] = scheduler.currentDate
-          throttleValues[id] = nil
-          return SignalProducer(value: value)
-        }
-
-        return SignalProducer(value: value)
-          .delay(
-            throttleTime.addingTimeInterval(interval).timeIntervalSince1970
+            .delay(
+              throttleTime.addingTimeInterval(interval).timeIntervalSince1970
               - scheduler.currentDate.timeIntervalSince1970,
-            on: scheduler
-          ).on(
-            value: { _ in
-              throttleLock.sync {
-                throttleTimes[id] = scheduler.currentDate
-                throttleValues[id] = nil
+              on: scheduler
+            ).on(
+              value: { _ in
+                throttleLock.sync {
+                  throttleTimes[id] = scheduler.currentDate
+                  throttleValues[id] = nil
+                }
               }
-            }
-          )
-      }
-      .eraseToEffect()
-      .cancellable(id: id, cancelInFlight: true)
+            )
+        }
+        .eraseToEffect()
+        .cancellable(id: id, cancelInFlight: true)
+    }
   }
 
   /// Throttles an effect so that it only publishes one output per given interval.
@@ -73,7 +78,7 @@ extension Effect {
   ///     the time system of the scheduler.
   ///   - scheduler: The scheduler you want to deliver the throttled output to.
   ///   - latest: A boolean value that indicates whether to publish the most recent element. If
-  ///     `false`, the publisher emits the first element received during the interval.
+  ///     `false`, the producer emits the first element received during the interval.
   /// - Returns: An effect that emits either the most-recent or first element received during the
   ///   specified interval.
   public func throttle(

Sources/ComposableArchitecture/Store.swift
@@ -366,44 +366,61 @@ public final class Store<State, Action> {
       let action = self.bufferedActions.removeFirst()
       let effect = self.reducer(&currentState, action)
 
-      var didComplete = false
-      let boxedTask = Box<Task<Void, Never>?>(wrappedValue: nil)
-      let uuid = UUID()
-      let observer = Signal<Action, Never>.Observer(
-        value: { [weak self] effectAction in
-          guard let self = self else { return }
-          if let task = self.send(effectAction, originatingFrom: action) {
-            tasks.wrappedValue.append(task)
-          }
-        },
-        completed: { [weak self] in
+      switch effect.operation {
+      case .none:
+        break
+      case let .producer(producer):
+        var didComplete = false
+        let boxedTask = Box<Task<Void, Never>?>(wrappedValue: nil)
+        let uuid = UUID()
+        let observer = Signal<Action, Never>.Observer(
+          value: { [weak self] effectAction in
+            guard let self = self else { return }
+            if let task = self.send(effectAction, originatingFrom: action) {
+              tasks.wrappedValue.append(task)
+            }
+          },
+          completed: { [weak self] in
+              self?.threadCheck(status: .effectCompletion(action))
+              boxedTask.wrappedValue?.cancel()
+              didComplete = true
+            self?.effectDisposables.removeValue(forKey: uuid)?.dispose()
+            },
+          interrupted: { [weak self] in
+            boxedTask.wrappedValue?.cancel()
+            didComplete = true
+            self?.effectDisposables.removeValue(forKey: uuid)?.dispose()
+              }
+        )
+
+        let effectDisposable = CompositeDisposable()
+        effectDisposable += producer.start(observer)
+        effectDisposable += AnyDisposable { [weak self] in
           self?.threadCheck(status: .effectCompletion(action))
-          boxedTask.wrappedValue?.cancel()
-          didComplete = true
           self?.effectDisposables.removeValue(forKey: uuid)?.dispose()
-        },
-        interrupted: { [weak self] in
-          boxedTask.wrappedValue?.cancel()
-          didComplete = true
-          self?.effectDisposables.removeValue(forKey: uuid)?.dispose()
-        }
-      )
+            }
 
-      let effectDisposable = CompositeDisposable()
-      effectDisposable += effect.producer.start(observer)
-      effectDisposable += AnyDisposable { [weak self] in
-        self?.threadCheck(status: .effectCompletion(action))
-        self?.effectDisposables.removeValue(forKey: uuid)?.dispose()
-      }
-
-      if !didComplete {
-        let task = Task<Void, Never> { @MainActor in
-          for await _ in AsyncStream<Void>.never {}
-          effectDisposable.dispose()
+        if !didComplete {
+          let task = Task<Void, Never> { @MainActor in
+            for await _ in AsyncStream<Void>.never {}
+            effectDisposable.dispose()
+          }
+          boxedTask.wrappedValue = task
+          tasks.wrappedValue.append(task)
+          self.effectDisposables[uuid] = effectDisposable
         }
-        boxedTask.wrappedValue = task
-        tasks.wrappedValue.append(task)
-        self.effectDisposables[uuid] = effectDisposable
+      case let .run(priority, operation):
+        tasks.wrappedValue.append(
+          Task(priority: priority) {
+            await operation(
+              Send {
+                if let task = self.send($0, originatingFrom: action) {
+                  tasks.wrappedValue.append(task)
+                }
+              }
+            )
+          }
+        )
       }
     }
 