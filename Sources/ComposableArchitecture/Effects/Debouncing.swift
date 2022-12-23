pointfreeco/swift-composable-architecture

Use publishers in `Effect.debounce` and `Effect.deferred` (#1331)

Sources/ComposableArchitecture/Effects/Debouncing.swift
@@ -24,6 +24,26 @@ extension Effect {
   ///   - scheduler: The scheduler you want to deliver the debounced output to.
   ///   - options: Scheduler options that customize the effect's delivery of elements.
   /// - Returns: An effect that publishes events only after a specified time elapses.
+  @available(
+    iOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
+  @available(
+    macOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
+  @available(
+    tvOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
+  @available(
+    watchOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
   public func debounce<S: Scheduler>(
     id: AnyHashable,
     for dueTime: S.SchedulerTimeType.Stride,
@@ -33,7 +53,7 @@ extension Effect {
     switch self.operation {
     case .none:
       return .none
-    case .publisher:
+    case .publisher, .run:
       return Self(
         operation: .publisher(
           Just(())
@@ -44,23 +64,6 @@ extension Effect {
         )
       )
       .cancellable(id: id, cancelInFlight: true)
-    case let .run(priority, operation):
-      return Self(
-        operation: .run(priority) { send in
-          await withTaskCancellation(id: id, cancelInFlight: true) {
-            do {
-              try await scheduler.sleep(for: dueTime, options: options)
-              await operation(
-                Send { output in
-                  scheduler.schedule {
-                    send(output)
-                  }
-                }
-              )
-            } catch {}
-          }
-        }
-      )
     }
   }
 

Sources/ComposableArchitecture/Effects/Deferring.swift
@@ -29,7 +29,7 @@ extension Effect {
     switch self.operation {
     case .none:
       return .none
-    case .publisher:
+    case .publisher, .run:
       return Self(
         operation: .publisher(
           Just(())
@@ -38,21 +38,6 @@ extension Effect {
             .flatMap { self.publisher.receive(on: scheduler) }
             .eraseToAnyPublisher()
         )
-      )
-    case let .run(priority, operation):
-      return Self(
-        operation: .run(priority) { send in
-          do {
-            try await scheduler.sleep(for: dueTime)
-            await operation(
-              Send { output in
-                scheduler.schedule {
-                  send(output)
-                }
-              }
-            )
-          } catch {}
-        }
       )
     }
   }

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit adf7cc8e365238c03ead4c48ba66afd5d2cacadc)
# Conflicts:
#	Sources/ComposableArchitecture/Effects/Debouncing.swift
#	Sources/ComposableArchitecture/Effects/Deferring.swift

Sources/ComposableArchitecture/Effects/Debouncing.swift
@@ -24,6 +24,26 @@ extension Effect {
   ///   - dueTime: The duration you want to debounce for.
   ///   - scheduler: The scheduler you want to deliver the debounced output to.
   /// - Returns: An effect that publishes events only after a specified time elapses.
+  @available(
+    iOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
+  @available(
+    macOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
+  @available(
+    tvOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
+  @available(
+    watchOS,
+    deprecated: 9999.0,
+    message: "Use 'withTaskCancellation(id: _, cancelInFlight: true)' in 'Effect.run', instead."
+  )
   public func debounce(
     id: AnyHashable,
     for dueTime: TimeInterval,
@@ -32,7 +52,7 @@ extension Effect {
     switch self.operation {
     case .none:
       return .none
-    case .producer:
+    case .producer, .run:
       return Self(
         operation: .producer(
           SignalProducer<Void, Never>.init(value: ())
@@ -42,23 +62,6 @@ extension Effect {
         )
       )
       .cancellable(id: id, cancelInFlight: true)
-    case let .run(priority, operation):
-      return Self(
-        operation: .run(priority) { send in
-          await withTaskCancellation(id: id, cancelInFlight: true) {
-            do {
-              try await scheduler.sleep(for: .nanoseconds(Int(dueTime * TimeInterval(NSEC_PER_SEC))))
-              await operation(
-                Send { output in
-                  scheduler.schedule {
-                    send(output)
-                  }
-                }
-              )
-            } catch {}
-          }
-        }
-      )
     }
   }
 

Sources/ComposableArchitecture/Effects/Deferring.swift
@@ -29,28 +29,13 @@ extension Effect {
     switch self.operation {
     case .none:
       return .none
-    case .producer:
+    case .producer, .run:
       return Self(
         operation: .producer(
           SignalProducer<Void, Never>(value: ())
             .delay(dueTime, on: scheduler)
             .flatMap(.latest) { self.producer.observe(on: scheduler) }
         )
-      )
-    case let .run(priority, operation):
-      return Self(
-        operation: .run(priority) { send in
-          do {
-            try await scheduler.sleep(for: .nanoseconds(Int(dueTime * TimeInterval(NSEC_PER_SEC))))
-            await operation(
-              Send { output in
-                scheduler.schedule {
-                  send(output)
-                }
-              }
-            )
-          } catch {}
-        }
       )
     }
   }