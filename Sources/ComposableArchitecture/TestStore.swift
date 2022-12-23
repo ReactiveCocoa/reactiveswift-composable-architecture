pointfreeco/swift-composable-architecture

Don't spin up long-living test effects for .none

Sources/ComposableArchitecture/TestStore.swift
@@ -263,22 +263,27 @@
             self.receivedActions.append((action, state))
           }
 
-          let effect = LongLivingEffect(file: action.file, line: action.line)
-          return
-            effects
-            .handleEvents(
-              receiveSubscription: { [effectDidSubscribe = self.effectDidSubscribe, weak self] _ in
-                self?.inFlightEffects.insert(effect)
-                Task {
-                  await Task.megaYield()
-                  effectDidSubscribe.continuation.yield()
-                }
-              },
-              receiveCompletion: { [weak self] _ in self?.inFlightEffects.remove(effect) },
-              receiveCancel: { [weak self] in self?.inFlightEffects.remove(effect) }
-            )
-            .eraseToEffect { .init(origin: .receive($0), file: action.file, line: action.line) }
-
+          switch effects.operation {
+          case .none:
+            return .none
+          case .publisher, .run:
+            let effect = LongLivingEffect(file: action.file, line: action.line)
+            return
+              effects
+              .handleEvents(
+                receiveSubscription: {
+                  [effectDidSubscribe = self.effectDidSubscribe, weak self] _ in
+                  self?.inFlightEffects.insert(effect)
+                  Task {
+                    await Task.megaYield()
+                    effectDidSubscribe.continuation.yield()
+                  }
+                },
+                receiveCompletion: { [weak self] _ in self?.inFlightEffects.remove(effect) },
+                receiveCancel: { [weak self] in self?.inFlightEffects.remove(effect) }
+              )
+              .eraseToEffect { .init(origin: .receive($0), file: action.file, line: action.line) }
+          }
         },
         environment: ()
       )

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit f94988ec8e1770fcb3dff8cacd643d945d20cd53)
# Conflicts:
#	Sources/ComposableArchitecture/TestStore.swift

Sources/ComposableArchitecture/TestStore.swift
@@ -267,23 +267,27 @@
             self.receivedActions.append((action, state))
           }
 
-          let effect = LongLivingEffect(file: action.file, line: action.line)
-          return
-            effects
-            .producer
-            .on(
-              starting: { [effectDidSubscribe = self.effectDidSubscribe, weak self] in
-                self?.inFlightEffects.insert(effect)
-                Task {
-                  await Task.megaYield()
-                  effectDidSubscribe.continuation.yield()
-                }
-              },
-              completed: { [weak self] in self?.inFlightEffects.remove(effect) },
-              disposed: { [weak self] in self?.inFlightEffects.remove(effect) }
-            )
-            .eraseToEffect { .init(origin: .receive($0), file: action.file, line: action.line) }
-
+          switch effects.operation {
+          case .none:
+            return .none
+          case .producer, .run:
+            let effect = LongLivingEffect(file: action.file, line: action.line)
+            return
+              effects
+              .producer
+              .on(
+                starting: { [effectDidSubscribe = self.effectDidSubscribe, weak self] in
+                  self?.inFlightEffects.insert(effect)
+                  Task {
+                    await Task.megaYield()
+                    effectDidSubscribe.continuation.yield()
+                  }
+                },
+                completed: { [weak self] in self?.inFlightEffects.remove(effect) },
+                disposed: { [weak self] in self?.inFlightEffects.remove(effect) }
+              )
+              .eraseToEffect { .init(origin: .receive($0), file: action.file, line: action.line) }
+          }
         },
         environment: ()
       )