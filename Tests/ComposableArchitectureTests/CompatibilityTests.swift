pointfreeco/swift-composable-architecture

allow processing of sync actions resulting from state update (#1360)
* allow processing of sync actions resulting from state update

Sending an action into the system synchronously from a ViewStore's publisher
can result in state updates publishing the wrong, old, value.

This PR fixes it by keeping the `Store` in `sending` state while
we publish state updates. Once that's done, we check if there
were any new actions coming in, in which case we feed them back and
start processing again.

* Clean up and clarify behavior

Co-authored-by: Stephen Celis <stephen@stephencelis.com>

Tests/ComposableArchitectureTests/CompatibilityTests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 @MainActor
 final class CompatibilityTests: XCTestCase {
-  func testCaseStudy_ReentrantEffect() {
+  func testCaseStudy_ReentrantActionsFromBuffer() {
     let cancelID = UUID()
 
     struct State: Equatable {}
@@ -73,6 +73,57 @@ final class CompatibilityTests: XCTestCase {
         "stop",
       ]
     )
+  }
+
+  func testCaseStudy_ReentrantActionsFromPublisher() {
+    struct State: Equatable {
+      var city: String
+      var country: String
+    }
+
+    enum Action: Equatable {
+      case updateCity(String)
+      case updateCountry(String)
+    }
+
+    let reducer = Reducer<State, Action, Void> { state, action, _ in
+      switch action {
+      case let .updateCity(city):
+        state.city = city
+        return .none
+      case let .updateCountry(country):
+        state.country = country
+        return .none
+      }
+    }
+
+    let store = Store(
+      initialState: State(city: "New York", country: "USA"),
+      reducer: reducer,
+      environment: ()
+    )
+    let viewStore = ViewStore(store)
+
+    var cancellables: Set<AnyCancellable> = []
+
+    viewStore.publisher.city
+      .sink { city in
+        if city == "London" {
+          viewStore.send(.updateCountry("UK"))
+        }
+      }
+      .store(in: &cancellables)
+
+    var countryUpdates = [String]()
+    viewStore.publisher.country
+      .sink { country in
+        countryUpdates.append(country)
+      }
+      .store(in: &cancellables)
+
+    viewStore.send(.updateCity("London"))
+
+    XCTAssertEqual(countryUpdates, ["USA", "UK"])
   }
 }
 

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit 0855c9952549173fc7c655fcee31ffbb709e2dac)
# Conflicts:
#	Sources/ComposableArchitecture/Store.swift

Tests/ComposableArchitectureTests/CompatibilityTests.swift
@@ -4,7 +4,7 @@ import XCTest
 
 @MainActor
 final class CompatibilityTests: XCTestCase {
-  func testCaseStudy_ReentrantEffect() {
+  func testCaseStudy_ReentrantActionsFromBuffer() {
     let cancelID = UUID()
 
     struct State: Equatable {}
@@ -71,6 +71,53 @@ final class CompatibilityTests: XCTestCase {
         "stop",
       ]
     )
+  }
+
+  func testCaseStudy_ReentrantActionsFromProducer() {
+    struct State: Equatable {
+      var city: String
+      var country: String
+    }
+
+    enum Action: Equatable {
+      case updateCity(String)
+      case updateCountry(String)
+    }
+
+    let reducer = Reducer<State, Action, Void> { state, action, _ in
+      switch action {
+      case let .updateCity(city):
+        state.city = city
+        return .none
+      case let .updateCountry(country):
+        state.country = country
+        return .none
+      }
+    }
+
+    let store = Store(
+      initialState: State(city: "New York", country: "USA"),
+      reducer: reducer,
+      environment: ()
+    )
+    let viewStore = ViewStore(store)
+
+    viewStore.produced.city
+      .startWithValues { city in
+        if city == "London" {
+          viewStore.send(.updateCountry("UK"))
+        }
+      }
+
+    var countryUpdates = [String]()
+    viewStore.produced.country
+      .startWithValues { country in
+        countryUpdates.append(country)
+      }
+
+    viewStore.send(.updateCity("London"))
+
+    XCTAssertEqual(countryUpdates, ["USA", "UK"])
   }
 }
 