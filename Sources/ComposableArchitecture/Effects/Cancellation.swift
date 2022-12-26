pointfreeco/swift-composable-architecture

Fix task cancellation leak (#1418)
* Fix leak in withTaskCancellation(id:)

* Add failing test that is fixed

* wip

* clean up

* update tests

* wip

* wip

* wip

* wip

* clean up

* fix 13.4

* try to fix test

Sources/ComposableArchitecture/Effects/Cancellation.swift
@@ -196,27 +196,24 @@ public func withTaskCancellation<T: Sendable>(
   cancelInFlight: Bool = false,
   operation: @Sendable @escaping () async throws -> T
 ) async rethrows -> T {
-  let task = { () -> Task<T, Error> in
-    cancellablesLock.lock()
-    let id = CancelToken(id: id)
+  let id = CancelToken(id: id)
+  let (cancellable, task) = cancellablesLock.sync { () -> (AnyCancellable, Task<T, Error>) in
     if cancelInFlight {
       cancellationCancellables[id]?.forEach { $0.cancel() }
     }
     let task = Task { try await operation() }
-    var cancellable: AnyCancellable!
-    cancellable = AnyCancellable {
-      task.cancel()
-      cancellablesLock.sync {
-        cancellationCancellables[id]?.remove(cancellable)
-        if cancellationCancellables[id]?.isEmpty == .some(true) {
-          cancellationCancellables[id] = nil
-        }
+    let cancellable = AnyCancellable { task.cancel() }
+    cancellationCancellables[id, default: []].insert(cancellable)
+    return (cancellable, task)
+  }
+  defer {
+    cancellablesLock.sync {
+      cancellationCancellables[id]?.remove(cancellable)
+      if cancellationCancellables[id]?.isEmpty == .some(true) {
+        cancellationCancellables[id] = nil
       }
     }
-    cancellationCancellables[id, default: []].insert(cancellable)
-    cancellablesLock.unlock()
-    return task
-  }()
+  }
   do {
     return try await task.cancellableValue
   } catch {
@@ -252,10 +249,8 @@ extension Task where Success == Never, Failure == Never {
   /// Cancel any currently in-flight operation with the given identifier.
   ///
   /// - Parameter id: An identifier.
-  public static func cancel<ID: Hashable & Sendable>(id: ID) async {
-    await MainActor.run {
-      cancellablesLock.sync { cancellationCancellables[.init(id: id)]?.forEach { $0.cancel() } }
-    }
+  public static func cancel<ID: Hashable & Sendable>(id: ID) {
+    cancellablesLock.sync { cancellationCancellables[.init(id: id)]?.forEach { $0.cancel() } }
   }
 
   /// Cancel any currently in-flight operation with the given identifier.
@@ -264,8 +259,8 @@ extension Task where Success == Never, Failure == Never {
   /// identifier.
   ///
   /// - Parameter id: A unique type identifying the operation.
-  public static func cancel(id: Any.Type) async {
-    await self.cancel(id: ObjectIdentifier(id))
+  public static func cancel(id: Any.Type) {
+    self.cancel(id: ObjectIdentifier(id))
   }
 }
 

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit bc6f87dc44d85cccc74e49b017557577ecc44ca4)
# Conflicts:
#	Sources/ComposableArchitecture/Effects/Cancellation.swift
#	Tests/ComposableArchitectureTests/EffectRunTests.swift
#	Tests/ComposableArchitectureTests/EffectTaskTests.swift

Sources/ComposableArchitecture/Effects/Cancellation.swift
@@ -208,27 +208,24 @@ public func withTaskCancellation<T: Sendable>(
   cancelInFlight: Bool = false,
   operation: @Sendable @escaping () async throws -> T
 ) async rethrows -> T {
-  let task = { () -> Task<T, Error> in
-    cancellablesLock.lock()
-    let id = CancelToken(id: id)
+  let id = CancelToken(id: id)
+  let (cancellable, task) = cancellablesLock.sync { () -> (AnyDisposable, Task<T, Error>) in
     if cancelInFlight {
       cancellationCancellables[id]?.forEach { $0.dispose() }
     }
     let task = Task { try await operation() }
-    var cancellable: AnyDisposable!
-    cancellable = AnyDisposable {
-      task.cancel()
-      cancellablesLock.sync {
-        cancellationCancellables[id]?.remove(cancellable)
-        if cancellationCancellables[id]?.isEmpty == .some(true) {
-          cancellationCancellables[id] = nil
-        }
+    let cancellable = AnyDisposable { task.cancel() }
+    cancellationCancellables[id, default: []].insert(cancellable)
+    return (cancellable, task)
+  }
+  defer {
+    cancellablesLock.sync {
+      cancellationCancellables[id]?.remove(cancellable)
+      if cancellationCancellables[id]?.isEmpty == .some(true) {
+        cancellationCancellables[id] = nil
       }
     }
-    cancellationCancellables[id, default: []].insert(cancellable)
-    cancellablesLock.unlock()
-    return task
-  }()
+  }
   do {
     return try await task.cancellableValue
   } catch {
@@ -264,10 +261,8 @@ extension Task where Success == Never, Failure == Never {
   /// Cancel any currently in-flight operation with the given identifier.
   ///
   /// - Parameter id: An identifier.
-  public static func cancel<ID: Hashable & Sendable>(id: ID) async {
-    await MainActor.run {
-      cancellablesLock.sync { cancellationCancellables[.init(id: id)]?.forEach { $0.dispose() } }
-    }
+  public static func cancel<ID: Hashable & Sendable>(id: ID) {
+    cancellablesLock.sync { cancellationCancellables[.init(id: id)]?.forEach { $0.dispose() } }
   }
 
   /// Cancel any currently in-flight operation with the given identifier.
@@ -276,8 +271,8 @@ extension Task where Success == Never, Failure == Never {
   /// identifier.
   ///
   /// - Parameter id: A unique type identifying the operation.
-  public static func cancel(id: Any.Type) async {
-    await self.cancel(id: ObjectIdentifier(id))
+  public static func cancel(id: Any.Type) {
+    self.cancel(id: ObjectIdentifier(id))
   }
 }
 