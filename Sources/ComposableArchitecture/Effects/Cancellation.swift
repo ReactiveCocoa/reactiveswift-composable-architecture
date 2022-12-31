pointfreeco/swift-composable-architecture

Use @_spi to test more internals in release. (#1456)

Sources/ComposableArchitecture/Effects/Cancellation.swift
@@ -42,24 +42,24 @@ extension Effect {
                   AnyPublisher<Action, Failure>, PassthroughSubject<Void, Never>
                 >
               > in
-            cancellablesLock.lock()
-            defer { cancellablesLock.unlock() }
+            _cancellablesLock.lock()
+            defer { _cancellablesLock.unlock() }
 
-            let id = CancelToken(id: id)
+            let id = _CancelToken(id: id)
             if cancelInFlight {
-              cancellationCancellables[id]?.forEach { $0.cancel() }
+              _cancellationCancellables[id]?.forEach { $0.cancel() }
             }
 
             let cancellationSubject = PassthroughSubject<Void, Never>()
 
             var cancellationCancellable: AnyCancellable!
             cancellationCancellable = AnyCancellable {
-              cancellablesLock.sync {
+              _cancellablesLock.sync {
                 cancellationSubject.send(())
                 cancellationSubject.send(completion: .finished)
-                cancellationCancellables[id]?.remove(cancellationCancellable)
-                if cancellationCancellables[id]?.isEmpty == .some(true) {
-                  cancellationCancellables[id] = nil
+                _cancellationCancellables[id]?.remove(cancellationCancellable)
+                if _cancellationCancellables[id]?.isEmpty == .some(true) {
+                  _cancellationCancellables[id] = nil
                 }
               }
             }
@@ -67,8 +67,8 @@ extension Effect {
             return publisher.prefix(untilOutputFrom: cancellationSubject)
               .handleEvents(
                 receiveSubscription: { _ in
-                  _ = cancellablesLock.sync {
-                    cancellationCancellables[id, default: []].insert(
+                  _ = _cancellablesLock.sync {
+                    _cancellationCancellables[id, default: []].insert(
                       cancellationCancellable
                     )
                   }
@@ -112,8 +112,8 @@ extension Effect {
   ///   identifier.
   public static func cancel(id: AnyHashable) -> Self {
     .fireAndForget {
-      cancellablesLock.sync {
-        cancellationCancellables[.init(id: id)]?.forEach { $0.cancel() }
+      _cancellablesLock.sync {
+        _cancellationCancellables[.init(id: id)]?.forEach { $0.cancel() }
       }
     }
   }
@@ -196,21 +196,21 @@ public func withTaskCancellation<T: Sendable>(
   cancelInFlight: Bool = false,
   operation: @Sendable @escaping () async throws -> T
 ) async rethrows -> T {
-  let id = CancelToken(id: id)
-  let (cancellable, task) = cancellablesLock.sync { () -> (AnyCancellable, Task<T, Error>) in
+  let id = _CancelToken(id: id)
+  let (cancellable, task) = _cancellablesLock.sync { () -> (AnyCancellable, Task<T, Error>) in
     if cancelInFlight {
-      cancellationCancellables[id]?.forEach { $0.cancel() }
+      _cancellationCancellables[id]?.forEach { $0.cancel() }
     }
     let task = Task { try await operation() }
     let cancellable = AnyCancellable { task.cancel() }
-    cancellationCancellables[id, default: []].insert(cancellable)
+    _cancellationCancellables[id, default: []].insert(cancellable)
     return (cancellable, task)
   }
   defer {
-    cancellablesLock.sync {
-      cancellationCancellables[id]?.remove(cancellable)
-      if cancellationCancellables[id]?.isEmpty == .some(true) {
-        cancellationCancellables[id] = nil
+    _cancellablesLock.sync {
+      _cancellationCancellables[id]?.remove(cancellable)
+      if _cancellationCancellables[id]?.isEmpty == .some(true) {
+        _cancellationCancellables[id] = nil
       }
     }
   }
@@ -250,7 +250,7 @@ extension Task where Success == Never, Failure == Never {
   ///
   /// - Parameter id: An identifier.
   public static func cancel<ID: Hashable & Sendable>(id: ID) {
-    cancellablesLock.sync { cancellationCancellables[.init(id: id)]?.forEach { $0.cancel() } }
+    _cancellablesLock.sync { _cancellationCancellables[.init(id: id)]?.forEach { $0.cancel() } }
   }
 
   /// Cancel any currently in-flight operation with the given identifier.
@@ -264,18 +264,18 @@ extension Task where Success == Never, Failure == Never {
   }
 }
 
-struct CancelToken: Hashable {
+@_spi(Internals) public struct _CancelToken: Hashable {
   let id: AnyHashable
   let discriminator: ObjectIdentifier
 
-  init(id: AnyHashable) {
+  public init(id: AnyHashable) {
     self.id = id
     self.discriminator = ObjectIdentifier(type(of: id.base))
   }
 }
 
-var cancellationCancellables: [CancelToken: Set<AnyCancellable>] = [:]
-let cancellablesLock = NSRecursiveLock()
+@_spi(Internals) public var _cancellationCancellables: [_CancelToken: Set<AnyCancellable>] = [:]
+@_spi(Internals) public let _cancellablesLock = NSRecursiveLock()
 
 @rethrows
 private protocol _ErrorMechanism {

Sources/ComposableArchitecture/Store.swift
@@ -122,7 +122,7 @@ import Foundation
 /// store are also checked to make sure that work is performed on the main thread.
 public final class Store<State, Action> {
   private var bufferedActions: [Action] = []
-  var effectCancellables: [UUID: AnyCancellable] = [:]
+  @_spi(Internals) public var effectCancellables: [UUID: AnyCancellable] = [:]
   private var isSending = false
   var parentCancellable: AnyCancellable?
   #if swift(>=5.7)
@@ -317,7 +317,7 @@ public final class Store<State, Action> {
     self.scope(state: toChildState, action: { $0 })
   }
 
-  func send(
+  @_spi(Internals) public func send(
     _ action: Action,
     originatingFrom originatingAction: Action? = nil
   ) -> Task<Void, Never>? {

Tests/ComposableArchitectureTests/StoreTests.swift
@@ -1,58 +1,49 @@
 import Combine
+@_spi(Internals) import ComposableArchitecture
 import XCTest
-
-#if DEBUG
-  @testable import ComposableArchitecture
-#else
-  import ComposableArchitecture
-#endif
 
 @MainActor
 final class StoreTests: XCTestCase {
   var cancellables: Set<AnyCancellable> = []
 
-  #if DEBUG
-    func testCancellableIsRemovedOnImmediatelyCompletingEffect() {
-      let store = Store(initialState: (), reducer: EmptyReducer<Void, Void>())
+  func testCancellableIsRemovedOnImmediatelyCompletingEffect() {
+    let store = Store(initialState: (), reducer: EmptyReducer<Void, Void>())
 
-      XCTAssertEqual(store.effectCancellables.count, 0)
+    XCTAssertEqual(store.effectCancellables.count, 0)
 
-      _ = store.send(())
+    _ = store.send(())
 
-      XCTAssertEqual(store.effectCancellables.count, 0)
-    }
-  #endif
+    XCTAssertEqual(store.effectCancellables.count, 0)
+  }
 
-  #if DEBUG
-    func testCancellableIsRemovedWhenEffectCompletes() {
-      let mainQueue = DispatchQueue.test
-      let effect = Effect<Void, Never>(value: ())
-        .delay(for: 1, scheduler: mainQueue)
-        .eraseToEffect()
+  func testCancellableIsRemovedWhenEffectCompletes() {
+    let mainQueue = DispatchQueue.test
+    let effect = Effect<Void, Never>(value: ())
+      .delay(for: 1, scheduler: mainQueue)
+      .eraseToEffect()
 
-      enum Action { case start, end }
+    enum Action { case start, end }
 
-      let reducer = Reduce<Void, Action>({ _, action in
-        switch action {
-        case .start:
-          return effect.map { .end }
-        case .end:
-          return .none
-        }
-      })
-      let store = Store(initialState: (), reducer: reducer)
+    let reducer = Reduce<Void, Action>({ _, action in
+      switch action {
+      case .start:
+        return effect.map { .end }
+      case .end:
+        return .none
+      }
+    })
+    let store = Store(initialState: (), reducer: reducer)
 
-      XCTAssertEqual(store.effectCancellables.count, 0)
+    XCTAssertEqual(store.effectCancellables.count, 0)
 
-      _ = store.send(.start)
+    _ = store.send(.start)
 
-      XCTAssertEqual(store.effectCancellables.count, 1)
+    XCTAssertEqual(store.effectCancellables.count, 1)
 
-      mainQueue.advance(by: 2)
+    mainQueue.advance(by: 2)
 
-      XCTAssertEqual(store.effectCancellables.count, 0)
-    }
-  #endif
+    XCTAssertEqual(store.effectCancellables.count, 0)
+  }
 
   func testScopedStoreReceivesUpdatesFromParent() {
     let counterReducer = Reduce<Int, Void>({ state, _ in
@@ -499,26 +490,24 @@ final class StoreTests: XCTestCase {
     await store.send(.task).cancel()
   }
 
-  #if DEBUG
-    func testScopeCancellation() async throws {
-      let neverEndingTask = Task<Void, Error> { try await Task.never() }
+  func testScopeCancellation() async throws {
+    let neverEndingTask = Task<Void, Error> { try await Task.never() }
 
-      let store = Store(
-        initialState: (),
-        reducer: Reduce<Void, Void>({ _, _ in
-          .fireAndForget {
-            try await neverEndingTask.value
-          }
-        })
-      )
-      let scopedStore = store.scope(state: { $0 })
+    let store = Store(
+      initialState: (),
+      reducer: Reduce<Void, Void>({ _, _ in
+        .fireAndForget {
+          try await neverEndingTask.value
+        }
+      })
+    )
+    let scopedStore = store.scope(state: { $0 })
 
-      let sendTask = scopedStore.send(())
-      await Task.yield()
-      neverEndingTask.cancel()
-      try await XCTUnwrap(sendTask).value
-      XCTAssertEqual(store.effectCancellables.count, 0)
-      XCTAssertEqual(scopedStore.effectCancellables.count, 0)
-    }
-  #endif
+    let sendTask = scopedStore.send(())
+    await Task.yield()
+    neverEndingTask.cancel()
+    try await XCTUnwrap(sendTask).value
+    XCTAssertEqual(store.effectCancellables.count, 0)
+    XCTAssertEqual(scopedStore.effectCancellables.count, 0)
+  }
 }

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit 16b47009b9ac72f4d82e16edf242adca8a9380d2)
# Conflicts:
#	Sources/ComposableArchitecture/Effects/Cancellation.swift
#	Sources/ComposableArchitecture/Internal/TaskCancellableValue.swift
#	Sources/ComposableArchitecture/Store.swift
#	Tests/ComposableArchitectureTests/EffectCancellationTests.swift
#	Tests/ComposableArchitectureTests/StoreTests.swift
#	Tests/ComposableArchitectureTests/TaskCancellationTests.swift

Sources/ComposableArchitecture/Effects/Cancellation.swift
@@ -46,12 +46,12 @@ extension Effect {
       return Self(
         operation: .producer(
           SignalProducer.deferred { () -> SignalProducer<Action, Failure> in
-            cancellablesLock.lock()
-            defer { cancellablesLock.unlock() }
+            _cancellablesLock.lock()
+            defer { _cancellablesLock.unlock() }
 
-            let id = CancelToken(id: id)
+            let id = _CancelToken(id: id)
             if cancelInFlight {
-              cancellationCancellables[id]?.forEach { $0.dispose() }
+              _cancellationCancellables[id]?.forEach { $0.dispose() }
             }
 
             let subject = Signal<Action, Failure>.pipe()
@@ -66,17 +66,17 @@ extension Effect {
               .start(subject.input)
             var cancellationDisposable: AnyDisposable!
             cancellationDisposable = AnyDisposable {
-              cancellablesLock.sync {
+              _cancellablesLock.sync {
                 subject.input.sendCompleted()
                 disposable.dispose()
-                cancellationCancellables[id]?.remove(cancellationDisposable)
-                if cancellationCancellables[id]?.isEmpty == .some(true) {
-                  cancellationCancellables[id] = nil
+                _cancellationCancellables[id]?.remove(cancellationDisposable)
+                if _cancellationCancellables[id]?.isEmpty == .some(true) {
+                  _cancellationCancellables[id] = nil
                 }
               }
             }
 
-            cancellationCancellables[id, default: []].insert(
+            _cancellationCancellables[id, default: []].insert(
               cancellationDisposable
             )
 
@@ -124,8 +124,8 @@ extension Effect {
   ///   identifier.
   public static func cancel(id: AnyHashable) -> Self {
     .fireAndForget {
-      cancellablesLock.sync {
-        cancellationCancellables[.init(id: id)]?.forEach { $0.dispose() }
+      _cancellablesLock.sync {
+        _cancellationCancellables[.init(id: id)]?.forEach { $0.dispose() }
       }
     }
   }
@@ -208,21 +208,21 @@ public func withTaskCancellation<T: Sendable>(
   cancelInFlight: Bool = false,
   operation: @Sendable @escaping () async throws -> T
 ) async rethrows -> T {
-  let id = CancelToken(id: id)
-  let (cancellable, task) = cancellablesLock.sync { () -> (AnyDisposable, Task<T, Error>) in
+  let id = _CancelToken(id: id)
+  let (cancellable, task) = _cancellablesLock.sync { () -> (AnyDisposable, Task<T, Error>) in
     if cancelInFlight {
-      cancellationCancellables[id]?.forEach { $0.dispose() }
+      _cancellationCancellables[id]?.forEach { $0.dispose() }
     }
     let task = Task { try await operation() }
     let cancellable = AnyDisposable { task.cancel() }
-    cancellationCancellables[id, default: []].insert(cancellable)
+    _cancellationCancellables[id, default: []].insert(cancellable)
     return (cancellable, task)
   }
   defer {
-    cancellablesLock.sync {
-      cancellationCancellables[id]?.remove(cancellable)
-      if cancellationCancellables[id]?.isEmpty == .some(true) {
-        cancellationCancellables[id] = nil
+    _cancellablesLock.sync {
+      _cancellationCancellables[id]?.remove(cancellable)
+      if _cancellationCancellables[id]?.isEmpty == .some(true) {
+        _cancellationCancellables[id] = nil
       }
     }
   }
@@ -262,7 +262,7 @@ extension Task where Success == Never, Failure == Never {
   ///
   /// - Parameter id: An identifier.
   public static func cancel<ID: Hashable & Sendable>(id: ID) {
-    cancellablesLock.sync { cancellationCancellables[.init(id: id)]?.forEach { $0.dispose() } }
+    _cancellablesLock.sync { _cancellationCancellables[.init(id: id)]?.forEach { $0.dispose() } }
   }
 
   /// Cancel any currently in-flight operation with the given identifier.
@@ -276,18 +276,18 @@ extension Task where Success == Never, Failure == Never {
   }
 }
 
-struct CancelToken: Hashable {
+@_spi(Internals) public struct _CancelToken: Hashable {
   let id: AnyHashable
   let discriminator: ObjectIdentifier
 
-  init(id: AnyHashable) {
+  public init(id: AnyHashable) {
     self.id = id
     self.discriminator = ObjectIdentifier(type(of: id.base))
   }
 }
 
-var cancellationCancellables: [CancelToken: Set<AnyDisposable>] = [:]
-let cancellablesLock = NSRecursiveLock()
+@_spi(Internals) public var _cancellationCancellables: [_CancelToken: Set<AnyDisposable>] = [:]
+@_spi(Internals) public let _cancellablesLock = NSRecursiveLock()
 
 @rethrows
 private protocol _ErrorMechanism {

Sources/ComposableArchitecture/Store.swift
@@ -122,7 +122,7 @@ import ReactiveSwift
 /// store are also checked to make sure that work is performed on the main thread.
 public final class Store<State, Action> {
   private var bufferedActions: [Action] = []
-  var effectDisposables: [UUID: Disposable] = [:]
+  @_spi(Internals) public var effectDisposables: [UUID: Disposable] = [:]
   private var isSending = false
   var parentDisposable: Disposable?
   #if swift(>=5.7)
@@ -325,7 +325,7 @@ public final class Store<State, Action> {
     self.scope(state: toChildState, action: { $0 })
   }
 
-  func send(
+  @_spi(Internals) public func send(
     _ action: Action,
     originatingFrom originatingAction: Action? = nil
   ) -> Task<Void, Never>? {

Tests/ComposableArchitectureTests/StoreTests.swift
@@ -1,11 +1,6 @@
+@_spi(Internals) import ComposableArchitecture
 import ReactiveSwift
 import XCTest
-
-#if DEBUG
-  @testable import ComposableArchitecture
-#else
-  import ComposableArchitecture
-#endif
 
 // `@MainActor` introduces issues gathering tests on Linux
 #if !os(Linux)
@@ -55,35 +50,33 @@ import XCTest
       }
     #endif
 
-    #if DEBUG
-      func testCancellableIsRemovedWhenEffectCompletes() {
-        let mainQueue = TestScheduler()
-        let effect = Effect<Void, Never>(value: ())
-          .deferred(for: 1, scheduler: mainQueue)
+    func testCancellableIsRemovedWhenEffectCompletes() {
+      let mainQueue = TestScheduler()
+      let effect = Effect<Void, Never>(value: ())
+        .deferred(for: 1, scheduler: mainQueue)
 
-        enum Action { case start, end }
+      enum Action { case start, end }
 
-        let reducer = Reduce<Void, Action>({ _, action in
-          switch action {
-          case .start:
-            return effect.map { .end }
-          case .end:
-            return .none
-          }
-        })
-        let store = Store(initialState: (), reducer: reducer)
+      let reducer = Reduce<Void, Action>({ _, action in
+        switch action {
+        case .start:
+          return effect.map { .end }
+        case .end:
+          return .none
+        }
+      })
+      let store = Store(initialState: (), reducer: reducer)
 
-        XCTAssertEqual(store.effectDisposables.count, 0)
+      XCTAssertEqual(store.effectDisposables.count, 0)
 
-        _ = store.send(.start)
+      _ = store.send(.start)
 
-        XCTAssertEqual(store.effectDisposables.count, 1)
+      XCTAssertEqual(store.effectDisposables.count, 1)
 
-        mainQueue.advance(by: 2)
+      mainQueue.advance(by: 2)
 
-        XCTAssertEqual(store.effectDisposables.count, 0)
-      }
-    #endif
+      XCTAssertEqual(store.effectDisposables.count, 0)
+    }
 
     func testScopedStoreReceivesUpdatesFromParent() {
       let counterReducer = Reduce<Int, Void>({ state, _ in
@@ -528,27 +521,25 @@ import XCTest
       await store.send(.task).cancel()
     }
 
-    #if DEBUG
-      func testScopeCancellation() async throws {
-        let neverEndingTask = Task<Void, Error> { try await Task.never() }
+    func testScopeCancellation() async throws {
+      let neverEndingTask = Task<Void, Error> { try await Task.never() }
 
-        let store = Store(
-          initialState: (),
-          reducer: Reduce<Void, Void>({ _, _ in
-            .fireAndForget {
-              try await neverEndingTask.value
-            }
-          })
-        )
-        let scopedStore = store.scope(state: { $0 })
+      let store = Store(
+        initialState: (),
+        reducer: Reduce<Void, Void>({ _, _ in
+          .fireAndForget {
+            try await neverEndingTask.value
+          }
+        })
+      )
+      let scopedStore = store.scope(state: { $0 })
 
-        let sendTask = scopedStore.send(())
-        await Task.yield()
-        neverEndingTask.cancel()
-        try await XCTUnwrap(sendTask).value
-        XCTAssertEqual(store.effectDisposables.count, 0)
-        XCTAssertEqual(scopedStore.effectDisposables.count, 0)
-      }
-    #endif
+      let sendTask = scopedStore.send(())
+      await Task.yield()
+      neverEndingTask.cancel()
+      try await XCTUnwrap(sendTask).value
+      XCTAssertEqual(store.effectDisposables.count, 0)
+      XCTAssertEqual(scopedStore.effectDisposables.count, 0)
+    }
   }
 #endif