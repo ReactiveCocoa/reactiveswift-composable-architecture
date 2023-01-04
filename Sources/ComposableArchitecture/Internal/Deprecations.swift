pointfreeco/swift-composable-architecture

Add a `EffectTask<Action>` typealias for `Effect<Action, Never>` and rename `Effect` to `EffectPublisher` (#1471)
* Add an `EffectOf<Action>` typealias for `Effect<Action, Never>`

* Fix doc

* Rename `EffectOf` to `EffectTask`

* Rename `Effect` to  `EffectPublisher`

* Soft-deprecate `Effect`

* Link to `EffectTask`

* Use `EffectPublisher` in Combine contexts

* Reword soft-deprecation message

* Remove `renamed:` fix-it for `Effect` deprecation

* Update Sources/ComposableArchitecture/Documentation.docc/Extensions/ReducerProtocol.md

Co-authored-by: Stephen Celis <stephen.celis@gmail.com>

* Update Sources/ComposableArchitecture/Documentation.docc/ComposableArchitecture.md

* Update Sources/ComposableArchitecture/Effect.swift

* Fix DocC identifiers

Co-authored-by: Stephen Celis <stephen.celis@gmail.com>

Sources/ComposableArchitecture/Internal/Deprecations.swift
@@ -482,19 +482,19 @@ extension TestStore {
 
 // MARK: - Deprecated after 0.38.2:
 
-extension Effect {
+extension EffectPublisher {
   @available(*, deprecated)
   public var upstream: AnyPublisher<Action, Failure> {
     self.publisher
   }
 }
 
-extension Effect where Failure == Error {
+extension EffectPublisher where Failure == Error {
   @_disfavoredOverload
   @available(
     *,
     deprecated,
-    message: "Use the non-failing version of 'Effect.task'"
+    message: "Use the non-failing version of 'EffectTask.task'"
   )
   public static func task(
     priority: TaskPriority? = nil,
@@ -552,7 +552,7 @@ extension Store {
 
 // MARK: - Deprecated after 0.38.0:
 
-extension Effect {
+extension EffectPublisher {
   @available(iOS, deprecated: 9999.0, renamed: "unimplemented")
   @available(macOS, deprecated: 9999.0, renamed: "unimplemented")
   @available(tvOS, deprecated: 9999.0, renamed: "unimplemented")
@@ -574,7 +574,7 @@ extension ViewStore {
 
 // MARK: - Deprecated after 0.34.0:
 
-extension Effect {
+extension EffectPublisher {
   @available(
     *,
     deprecated,

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit 41f1cb9f3f7ed046deaed1b838cbaf6899ee9983)
# Conflicts:
#	Sources/ComposableArchitecture/Documentation.docc/Articles/GettingStarted.md
#	Sources/ComposableArchitecture/Effect.swift
#	Sources/ComposableArchitecture/Effects/Animation.swift
#	Sources/ComposableArchitecture/Effects/ConcurrencySupport.swift
#	Sources/ComposableArchitecture/Effects/Publisher/Timer.swift
#	Sources/ComposableArchitecture/Effects/SignalProducer.swift
#	Sources/ComposableArchitecture/Internal/Create.swift
#	Sources/ComposableArchitecture/Internal/Deprecations.swift
#	Sources/ComposableArchitecture/Reducer/AnyReducer/AnyReducerSignpost.swift
#	Sources/ComposableArchitecture/Reducer/Reducers/SignpostReducer.swift
#	Sources/ComposableArchitecture/SwiftUI/Alert.swift
#	Sources/ComposableArchitecture/SwiftUI/ConfirmationDialog.swift
#	Sources/ComposableArchitecture/TestStore.swift
#	Sources/Dependencies/Dependencies/MainQueue.swift
#	Sources/Dependencies/Dependencies/MainRunLoop.swift
#	Tests/ComposableArchitectureTests/CompatibilityTests.swift
#	Tests/ComposableArchitectureTests/ComposableArchitectureTests.swift
#	Tests/ComposableArchitectureTests/EffectCancellationTests.swift
#	Tests/ComposableArchitectureTests/EffectRunTests.swift
#	Tests/ComposableArchitectureTests/EffectTaskTests.swift
#	Tests/ComposableArchitectureTests/EffectTests.swift
#	Tests/ComposableArchitectureTests/ReducerTests.swift
#	Tests/ComposableArchitectureTests/StoreTests.swift
#	Tests/ComposableArchitectureTests/TestStoreTests.swift
#	Tests/ComposableArchitectureTests/TimerTests.swift
#	Tests/ComposableArchitectureTests/ViewStoreTests.swift

Sources/ComposableArchitecture/Internal/Deprecations.swift
@@ -495,12 +495,12 @@ extension TestStore {
 
 // MARK: - Deprecated after 0.38.2:
 
-extension Effect where Failure == Error {
+extension EffectProducer where Failure == Error {
   @_disfavoredOverload
   @available(
     *,
     deprecated,
-    message: "Use the non-failing version of 'Effect.task'"
+    message: "Use the non-failing version of 'EffectTask.task'"
   )
   public static func task(
     priority: TaskPriority? = nil,
@@ -562,7 +562,7 @@ extension Store {
 
 // MARK: - Deprecated after 0.38.0:
 
-extension Effect {
+extension EffectProducer {
   @available(iOS, deprecated: 9999.0, renamed: "unimplemented")
   @available(macOS, deprecated: 9999.0, renamed: "unimplemented")
   @available(tvOS, deprecated: 9999.0, renamed: "unimplemented")
@@ -584,7 +584,7 @@ extension ViewStore {
 
 // MARK: - Deprecated after 0.34.0:
 
-extension Effect {
+extension EffectProducer {
   @available(
     *,
     deprecated,