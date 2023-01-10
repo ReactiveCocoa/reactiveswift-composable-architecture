7cfcca01c74eeeb0be2b980952cccdb1944a0668
Stephen Celis <stephen@stephencelis.com>
2023-01-10 17:43:43 -0500

    Remove and update a few deprecated, flakey tests (#1816)
    * Remove and update a few deprecated, flakey tests
    
    * wip
    
    (cherry picked from commit f2f3a2558d5d63108420b5413d2cc94a850f7b92)
    
    # Conflicts:
    #	Tests/ComposableArchitectureTests/BindingTests.swift
    #	Tests/ComposableArchitectureTests/CompatibilityTests.swift
    #	Tests/ComposableArchitectureTests/ComposableArchitectureTests.swift
    #	Tests/ComposableArchitectureTests/ReducerTests.swift
    #	Tests/ComposableArchitectureTests/RuntimeWarningTests.swift
    #	Tests/ComposableArchitectureTests/StoreTests.swift
    #	Tests/ComposableArchitectureTests/ViewStoreTests.swift

--- Tests/ComposableArchitectureTests/BindingTests.swift
+++ Tests/ComposableArchitectureTests/BindingTests.swift
@@ -1,46 +1,46 @@
 #if canImport(SwiftUI)
-  import ComposableArchitecture
-  import XCTest
+import ComposableArchitecture
+import XCTest
 
-  @MainActor
-  final class BindingTests: XCTestCase {
-    #if swift(>=5.7)
-      func testNestedBindableState() {
-        struct BindingTest: ReducerProtocol {
-          struct State: Equatable {
-            @BindableState var nested = Nested()
+@MainActor
+final class BindingTests: XCTestCase {
+  #if swift(>=5.7)
+    func testNestedBindableState() {
+      struct BindingTest: ReducerProtocol {
+        struct State: Equatable {
+          @BindableState var nested = Nested()
 
-            struct Nested: Equatable {
-              var field = ""
-            }
-          }
-
-          enum Action: BindableAction, Equatable {
-            case binding(BindingAction<State>)
-          }
-
-          var body: some ReducerProtocol<State, Action> {
-            BindingReducer()
-            Reduce { state, action in
-              switch action {
-              case .binding(\.$nested.field):
-                state.nested.field += "!"
-                return .none
-              default:
-                return .none
-              }
-            }
+          struct Nested: Equatable {
+            var field = ""
           }
         }
 
-        let store = Store(initialState: BindingTest.State(), reducer: BindingTest())
+        enum Action: BindableAction, Equatable {
+          case binding(BindingAction<State>)
+        }
 
-        let viewStore = ViewStore(store)
-
-        viewStore.binding(\.$nested.field).wrappedValue = "Hello"
-
-        XCTAssertEqual(viewStore.state, .init(nested: .init(field: "Hello!")))
+        var body: some ReducerProtocol<State, Action> {
+          BindingReducer()
+          Reduce { state, action in
+            switch action {
+            case .binding(\.$nested.field):
+              state.nested.field += "!"
+              return .none
+            default:
+              return .none
+            }
+          }
+        }
       }
-    #endif
-  }
+
+      let store = Store(initialState: BindingTest.State(), reducer: BindingTest())
+
+      let viewStore = ViewStore(store, observe: { $0 })
+
+      viewStore.binding(\.$nested.field).wrappedValue = "Hello"
+
+      XCTAssertEqual(viewStore.state, .init(nested: .init(field: "Hello!")))
+    }
+  #endif
+}
 #endif
