addfa9ab413f5244c2586f9c600d28af6e5cf36d
André Pacheco Neves <andre.neves@mindera.com>
2023-01-11 23:01:19 +0000

    Run swift-format

--- Tests/ComposableArchitectureTests/TestStoreFailureTests.swift
+++ Tests/ComposableArchitectureTests/TestStoreFailureTests.swift
@@ -49,7 +49,7 @@
           return .none
         }
       )
- 
+
       XCTExpectFailure {
         _ = store.send(()) { $0.count = 0 }
       } issueMatcher: {
@@ -267,7 +267,7 @@
     func testExpectedStateEqualityMustModify() async {
       let reducer = Reduce<Int, Bool> { state, action in
         switch action {
-        case true:  return Effect(value: false)
+        case true: return Effect(value: false)
         case false: return .none
         }
       }
