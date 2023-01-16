pointfreeco/swift-composable-architecture

Depend on swift-dependencies (#1808)
Co-authored-by: Brandon Williams <mbrandonw@hey.com>

Examples/TicTacToe/tic-tac-toe/Package.swift
@@ -27,7 +27,8 @@ let package = Package(
     .library(name: "TwoFactorUIKit", targets: ["TwoFactorUIKit"]),
   ],
   dependencies: [
-    .package(name: "swift-composable-architecture", path: "../../..")
+    .package(name: "swift-composable-architecture", path: "../../.."),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.0"),
   ],
   targets: [
     .target(
@@ -63,7 +64,7 @@ let package = Package(
     .target(
       name: "AuthenticationClient",
       dependencies: [
-        .product(name: "Dependencies", package: "swift-composable-architecture")
+        .product(name: "Dependencies", package: "swift-dependencies")
       ]
     ),
     .target(

Sources/ComposableArchitecture/Effect.swift
@@ -175,39 +175,41 @@ extension EffectPublisher where Failure == Never {
     fileID: StaticString = #fileID,
     line: UInt = #line
   ) -> Self {
-    let dependencies = DependencyValues._current
-    return Self(
-      operation: .run(priority) { send in
-        await DependencyValues.$_current.withValue(dependencies) {
-          do {
-            try await send(operation())
-          } catch is CancellationError {
-            return
-          } catch {
-            guard let handler = handler else {
-              #if DEBUG
-                var errorDump = ""
-                customDump(error, to: &errorDump, indent: 4)
-                runtimeWarn(
-                  """
-                  An "EffectTask.task" returned from "\(fileID):\(line)" threw an unhandled error. …
-
-                  \(errorDump)
-
-                  All non-cancellation errors must be explicitly handled via the "catch" parameter \
-                  on "EffectTask.task", or via a "do" block.
-                  """,
-                  file: file,
-                  line: line
-                )
-              #endif
+    withEscapedDependencies { escaped in
+      Self(
+        operation: .run(priority) { send in
+          await escaped.yield {
+            do {
+              try await send(operation())
+            } catch is CancellationError {
               return
+            } catch {
+              guard let handler = handler else {
+                #if DEBUG
+                  var errorDump = ""
+                  customDump(error, to: &errorDump, indent: 4)
+                  runtimeWarn(
+                    """
+                    An "EffectTask.task" returned from "\(fileID):\(line)" threw an unhandled \
+                    error. …
+
+                    \(errorDump)
+
+                    All non-cancellation errors must be explicitly handled via the "catch" \
+                    parameter on "EffectTask.task", or via a "do" block.
+                    """,
+                    file: file,
+                    line: line
+                  )
+                #endif
+                return
+              }
+              await send(handler(error))
             }
-            await send(handler(error))
           }
         }
-      }
-    )
+      )
+    }
   }
 
   /// Wraps an asynchronous unit of work that can emit any number of times in an effect.
@@ -257,39 +259,40 @@ extension EffectPublisher where Failure == Never {
     fileID: StaticString = #fileID,
     line: UInt = #line
   ) -> Self {
-    let dependencies = DependencyValues._current
-    return Self(
-      operation: .run(priority) { send in
-        await DependencyValues.$_current.withValue(dependencies) {
-          do {
-            try await operation(send)
-          } catch is CancellationError {
-            return
-          } catch {
-            guard let handler = handler else {
-              #if DEBUG
-                var errorDump = ""
-                customDump(error, to: &errorDump, indent: 4)
-                runtimeWarn(
-                  """
-                  An "EffectTask.run" returned from "\(fileID):\(line)" threw an unhandled error. …
-
-                  \(errorDump)
-
-                  All non-cancellation errors must be explicitly handled via the "catch" parameter \
-                  on "EffectTask.run", or via a "do" block.
-                  """,
-                  file: file,
-                  line: line
-                )
-              #endif
+    withEscapedDependencies { escaped in
+      Self(
+        operation: .run(priority) { send in
+          await escaped.yield {
+            do {
+              try await operation(send)
+            } catch is CancellationError {
               return
+            } catch {
+              guard let handler = handler else {
+                #if DEBUG
+                  var errorDump = ""
+                  customDump(error, to: &errorDump, indent: 4)
+                  runtimeWarn(
+                    """
+                    An "EffectTask.run" returned from "\(fileID):\(line)" threw an unhandled error. …
+
+                    \(errorDump)
+
+                    All non-cancellation errors must be explicitly handled via the "catch" parameter \
+                    on "EffectTask.run", or via a "do" block.
+                    """,
+                    file: file,
+                    line: line
+                  )
+                #endif
+                return
+              }
+              await handler(error, send)
             }
-            await handler(error, send)
           }
         }
-      }
-    )
+      )
+    }
   }
 
   /// Creates an effect that executes some work in the real world that doesn't need to feed data
@@ -500,23 +503,33 @@ extension EffectPublisher {
     case .none:
       return .none
     case let .publisher(publisher):
-      let dependencies = DependencyValues._current
-      let transform = { action in
-        DependencyValues.$_current.withValue(dependencies) {
-          transform(action)
-        }
-      }
-      return .init(operation: .publisher(publisher.map(transform).eraseToAnyPublisher()))
-    case let .run(priority, operation):
       return .init(
-        operation: .run(priority) { send in
-          await operation(
-            Send { action in
-              send(transform(action))
+        operation: .publisher(
+          publisher
+            .map(withEscapedDependencies { escaped in
+              { action in
+                escaped.yield {
+                  transform(action)
+                }
+              }
+            })
+            .eraseToAnyPublisher()
+        )
+      )
+    case let .run(priority, operation):
+      return withEscapedDependencies { escaped in
+          .init(
+            operation: .run(priority) { send in
+              await escaped.yield {
+                await operation(
+                  Send { action in
+                    send(transform(action))
+                  }
+                )
+              }
             }
           )
-        }
-      )
+      }
     }
   }
 }
@@ -651,7 +664,7 @@ extension EffectPublisher {
   message:
     """
     'Effect' has been deprecated in favor of 'EffectTask' when 'Failure == Never', or 'EffectPublisher<Output, Failure>' in general.
-    
+
     You are encouraged to use 'EffectTask<Action>' to model the output of your reducers, and to use Swift concurrency to model failable streams of values.
 
     To find and replace instances of 'Effect<Action, Never>' to 'EffectTask<Action, Never>' in your codebase, use the following regular expression:

ReactiveCocoa/reactiveswift-composable-architecture

(cherry picked from commit 9b5a510174ba956839035e8254d4be97f0c9ada1)
# Conflicts:
#	.github/workflows/ci.yml
#	ComposableArchitecture.xcworkspace/xcshareddata/swiftpm/Package.resolved
#	Examples/TicTacToe/tic-tac-toe/Package.swift
#	Package.resolved
#	Package.swift
#	README.md
#	Sources/ComposableArchitecture/Effect.swift
#	Sources/ComposableArchitecture/Effects/ConcurrencySupport.swift
#	Sources/ComposableArchitecture/Effects/SignalProducer.swift
#	Sources/Dependencies/Dependencies/MainQueue.swift
#	Sources/Dependencies/Dependencies/OpenURL.swift
#	Sources/Dependencies/Dependencies/RandomNumberGenerator.swift
#	Sources/Dependencies/Dependencies/UUID.swift
#	Sources/Dependencies/Internal/RuntimeWarnings.swift
#	Sources/swift-composable-architecture-benchmark/Dependencies.swift
#	Tests/ComposableArchitectureTests/EffectTests.swift
#	Tests/DependenciesTests/DependencyKeyTests.swift
#	Tests/DependenciesTests/DependencyValuesTests.swift

Examples/TicTacToe/tic-tac-toe/Package.swift
@@ -27,7 +27,8 @@ let package = Package(
     .library(name: "TwoFactorUIKit", targets: ["TwoFactorUIKit"]),
   ],
   dependencies: [
-    .package(name: "reactiveswift-composable-architecture", path: "../../..")
+    .package(name: "reactiveswift-composable-architecture", path: "../../.."),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.1.0"),
   ],
   targets: [
     .target(
@@ -36,7 +37,7 @@ let package = Package(
         "AuthenticationClient",
         "LoginCore",
         "NewGameCore",
-        .product(name: "ComposableArchitecture", package: "reactiveswift-composable-architecture"),
+        .product(name: "Dependencies", package: "swift-dependencies")
       ]
     ),
     .testTarget(
@@ -63,7 +64,7 @@ let package = Package(
     .target(
       name: "AuthenticationClient",
       dependencies: [
-        .product(name: "Dependencies", package: "reactiveswift-composable-architecture")
+        .product(name: "Dependencies", package: "swift-dependencies")
       ]
     ),
     .target(

Sources/ComposableArchitecture/Effect.swift
@@ -3,7 +3,7 @@ import ReactiveSwift
 import XCTestDynamicOverlay
 
 #if canImport(SwiftUI)
-  import SwiftUI
+import SwiftUI
 #endif
 
 /// This type is deprecated in favor of ``EffectTask``. See its documentation for more information.
@@ -178,10 +178,10 @@ extension EffectProducer where Failure == Never {
     fileID: StaticString = #fileID,
     line: UInt = #line
   ) -> Self {
-    let dependencies = DependencyValues._current
-    return Self(
+    withEscapedDependencies { escaped in
+      Self(
       operation: .run(priority) { send in
-        await DependencyValues.$_current.withValue(dependencies) {
+          await escaped.yield {
           do {
             try await send(operation())
           } catch is CancellationError {
@@ -193,12 +193,13 @@ extension EffectProducer where Failure == Never {
                 customDump(error, to: &errorDump, indent: 4)
                 runtimeWarn(
                   """
-                  An "EffectTask.task" returned from "\(fileID):\(line)" threw an unhandled error. …
+                  An "EffectTask.task" returned from "\(fileID):\(line)" threw an unhandled \
+                  error. …
 
                   \(errorDump)
 
-                  All non-cancellation errors must be explicitly handled via the "catch" parameter \
-                  on "EffectTask.task", or via a "do" block.
+                  All non-cancellation errors must be explicitly handled via the "catch" \
+                  parameter on "EffectTask.task", or via a "do" block.
                   """,
                   file: file,
                   line: line
@@ -211,6 +212,7 @@ extension EffectProducer where Failure == Never {
         }
       }
     )
+  }
   }
 
   /// Wraps an asynchronous unit of work that can emit any number of times in an effect.
@@ -260,10 +262,10 @@ extension EffectProducer where Failure == Never {
     fileID: StaticString = #fileID,
     line: UInt = #line
   ) -> Self {
-    let dependencies = DependencyValues._current
-    return Self(
+    withEscapedDependencies { escaped in
+      Self(
       operation: .run(priority) { send in
-        await DependencyValues.$_current.withValue(dependencies) {
+          await escaped.yield {
           do {
             try await operation(send)
           } catch is CancellationError {
@@ -293,6 +295,7 @@ extension EffectProducer where Failure == Never {
         }
       }
     )
+  }
   }
 
   /// Creates an effect that executes some work in the real world that doesn't need to feed data
@@ -370,17 +373,17 @@ public struct Send<Action> {
   }
 
   #if canImport(SwiftUI)
-    /// Sends an action back into the system from an effect with animation.
-    ///
-    /// - Parameters:
-    ///   - action: An action.
-    ///   - animation: An animation.
-    public func callAsFunction(_ action: Action, animation: Animation?) {
-      guard !Task.isCancelled else { return }
-      withAnimation(animation) {
-        self(action)
-      }
+  /// Sends an action back into the system from an effect with animation.
+  ///
+  /// - Parameters:
+  ///   - action: An action.
+  ///   - animation: An animation.
+  public func callAsFunction(_ action: Action, animation: Animation?) {
+    guard !Task.isCancelled else { return }
+    withAnimation(animation) {
+      self(action)
     }
+  }
   #endif
 }
 
@@ -505,25 +508,34 @@ extension EffectProducer {
     case .none:
       return .none
     case let .producer(producer):
-      let dependencies = DependencyValues._current
-      let transform = { action in
-        DependencyValues.$_current.withValue(dependencies) {
-          transform(action)
-        }
-      }
-      return .init(operation: .producer(producer.map(transform)))
-    case let .run(priority, operation):
       return .init(
+        operation: .producer(
+          producer
+            .map(withEscapedDependencies { escaped in
+              { action in
+                escaped.yield {
+                  transform(action)
+                }
+              }
+            })
+        )
+      )
+    case let .run(priority, operation):
+      return withEscapedDependencies { escaped in
+          .init(
         operation: .run(priority) { send in
+              await escaped.yield {
           await operation(
             Send { action in
               send(transform(action))
             }
           )
         }
+            }
       )
     }
   }
+}
 }
 
 // MARK: - Testing Effects