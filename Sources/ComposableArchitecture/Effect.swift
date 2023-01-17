24f5f401568328eff52a8be715282a120010437f
Stephen Celis <stephen@stephencelis.com>
2023-01-17 10:17:24 -0800

    Fix EffectTask deprecation
    (cherry picked from commit 9e5221a88ebad8d44aeb95643a58264fc13417b5)
    
    # Conflicts:
    #	Sources/ComposableArchitecture/Effect.swift

--- Sources/ComposableArchitecture/Effect.swift
+++ Sources/ComposableArchitecture/Effect.swift
@@ -3,7 +3,7 @@ import ReactiveSwift
 import XCTestDynamicOverlay
 
 #if canImport(SwiftUI)
-  import SwiftUI
+import SwiftUI
 #endif
 
 /// This type is deprecated in favor of ``EffectTask``. See its documentation for more information.
@@ -105,17 +105,17 @@ extension EffectProducer {
 /// the Combine interface to ``EffectProducer`` is considered soft deprecated, and you should
 /// eventually port to Swift's native concurrency tools.
 ///
-/// > Important: The publisher interface to ``EffectTask`` is considered deperecated, and you should
-/// try converting any uses of that interface to Swift's native concurrency tools.
+/// > Important: The publisher interface to ``EffectTask`` is considered deprecated, and you should
+/// > try converting any uses of that interface to Swift's native concurrency tools.
 /// >
 /// > Also, ``Store`` is not thread safe, and so all effects must receive values on the same
-/// thread. This is typically the main thread,  **and** if the store is being used to drive UI then
-/// it must receive values on the main thread.
+/// > thread. This is typically the main thread,  **and** if the store is being used to drive UI
+/// > then it must receive values on the main thread.
 /// >
 /// > This is only an issue if using the Combine interface of ``EffectProducer`` as mentioned
-/// above. If  you are using Swift's concurrency tools and the `.task`, `.run` and `.fireAndForget`
-/// functions on ``EffectTask``, then threading is automatically handled for you.
-public typealias EffectTask<Action> = Effect<Action, Never>
+/// > above. If  you are using Swift's concurrency tools and the `.task`, `.run`, and
+/// > `.fireAndForget` functions on ``EffectTask``, then threading is automatically handled for you.
+public typealias EffectTask<Action> = EffectPublisher<Action, Never>
 
 extension EffectProducer where Failure == Never {
   /// Wraps an asynchronous unit of work in an effect.
@@ -373,17 +373,17 @@ public struct Send<Action> {
   }
 
   #if canImport(SwiftUI)
-    /// Sends an action back into the system from an effect with animation.
-    ///
-    /// - Parameters:
-    ///   - action: An action.
-    ///   - animation: An animation.
-    public func callAsFunction(_ action: Action, animation: Animation?) {
-      guard !Task.isCancelled else { return }
-      withAnimation(animation) {
-        self(action)
-      }
+  /// Sends an action back into the system from an effect with animation.
+  ///
+  /// - Parameters:
+  ///   - action: An action.
+  ///   - animation: An animation.
+  public func callAsFunction(_ action: Action, animation: Animation?) {
+    guard !Task.isCancelled else { return }
+    withAnimation(animation) {
+      self(action)
     }
+  }
   #endif
 }
 
