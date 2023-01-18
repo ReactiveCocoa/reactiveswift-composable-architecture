dba87d1d27724340c9b21c689e9d3ab3c50805f5
Andrey Plotnikov <36012972+drucelweisse@users.noreply.github.com>
2023-01-18 06:05:29 +0300

    Add support for SwiftUI withTransaction API (#1824)
    * Adds withTransaction extension
    
    * reduce code
    
    * fix typo
    
    * Adds transaction support to Send and ViewStore.send
    
    * Update Sources/ComposableArchitecture/Effect.swift
    
    * Update Sources/ComposableArchitecture/ViewStore.swift
    
    Co-authored-by: Stephen Celis <stephen.celis@gmail.com>
    (cherry picked from commit b4e6e83c982fd715fd9049de205c46f91501fa1d)
    
    # Conflicts:
    #	Sources/ComposableArchitecture/Effect.swift
    #	Sources/ComposableArchitecture/Effects/Animation.swift

--- Sources/ComposableArchitecture/Effects/Animation.swift
+++ Sources/ComposableArchitecture/Effects/Animation.swift
@@ -1,32 +1,50 @@
 #if canImport(SwiftUI)
   import ReactiveSwift
-  import SwiftUI
+import SwiftUI
 
   extension EffectProducer {
-    /// Wraps the emission of each element with SwiftUI's `withAnimation`.
-    ///
-    /// ```swift
-    /// case .buttonTapped:
-    ///   return .task {
-    ///     .activityResponse(await self.apiClient.fetchActivity())
-    ///   }
-    ///   .animation()
-    /// ```
-    ///
-    /// - Parameter animation: An animation.
+  /// Wraps the emission of each element with SwiftUI's `withAnimation`.
+  ///
+  /// ```swift
+  /// case .buttonTapped:
+  ///   return .task {
+  ///     .activityResponse(await self.apiClient.fetchActivity())
+  ///   }
+  ///   .animation()
+  /// ```
+  ///
+  /// - Parameter animation: An animation.
     /// - Returns: An effect.
-    public func animation(_ animation: Animation? = .default) -> Self {
-      switch self.operation {
-      case .none:
-        return .none
+  public func animation(_ animation: Animation? = .default) -> Self {
+    self.transaction(Transaction(animation: animation))
+  }
+    
+  /// Wraps the emission of each element with SwiftUI's `withTransaction`.
+  ///
+  /// ```swift
+  /// case .buttonTapped:
+  ///   var transaction = Transaction(animation: .default)
+  ///   transaction.disablesAnimations = true
+  ///   return .task {
+  ///     .activityResponse(await self.apiClient.fetchActivity())
+  ///   }
+  ///   .transaction(transaction)
+  /// ```
+  ///
+  /// - Parameter transaction: A transaction.
+  /// - Returns: A publisher.
+  public func transaction(_ transaction: Transaction) -> Self {
+    switch self.operation {
+    case .none:
+      return .none
       case let .producer(producer):
-        return Self(
+      return Self(
           operation: .producer(
             SignalProducer<Action, Failure> { observer, _ in
               producer.start { action in
                 switch action {
                 case let .value(value):
-                  withAnimation(animation) {
+                  withTransaction(transaction) {
                     observer.send(value: value)
                   }
                 case .completed:
@@ -38,21 +56,21 @@
                 }
               }
             }
-          )
         )
-      case let .run(priority, operation):
-        return Self(
-          operation: .run(priority) { send in
-            await operation(
-              Send { value in
-                withAnimation(animation) {
-                  send(value)
-                }
+      )
+    case let .run(priority, operation):
+      return Self(
+        operation: .run(priority) { send in
+          await operation(
+            Send { value in
+              withTransaction(transaction) {
+                send(value)
               }
-            )
-          }
-        )
-      }
+            }
+          )
+        }
+      )
     }
   }
+}
 #endif
