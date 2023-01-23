b6bc66e9523a7cd82a50e8b579934ce5a1ff9cda
Stephen Celis <stephen@stephencelis.com>
2023-01-23 12:16:04 -0800

    Unconstrain TestStore action for predicate/case path `receive` (#1856)
    * Unconstrain TestStore action for predicate/case path `receive`
    
    These methods are currently defined in a constrained extension, but it's
    not necessary, so let's loosen the constraint.
    
    * added some tests
    
    * Update Sources/ComposableArchitecture/TestStore.swift
    
    * flakey test
    
    Co-authored-by: Brandon Williams <mbrandonw@hey.com>
    (cherry picked from commit b524b01be3dd7aa0220a4184cc1500ee7cb62ece)

--- Sources/ComposableArchitecture/ViewStore.swift
+++ Sources/ComposableArchitecture/ViewStore.swift
@@ -395,7 +395,7 @@ public final class ViewStore<ViewState, ViewAction> {
   /// - Parameters:
   ///   - action: An action.
   ///   - predicate: A predicate on `ViewState` that determines for how long this method should
-  ///                suspend.
+  ///     suspend.
   @MainActor
   public func send(_ action: ViewAction, while predicate: @escaping (ViewState) -> Bool) async {
     let task = self.send(action)
@@ -415,7 +415,7 @@ public final class ViewStore<ViewState, ViewAction> {
   ///   - action: An action.
   ///   - animation: The animation to perform when the action is sent.
   ///   - predicate: A predicate on `ViewState` that determines for how long this method should
-  ///                suspend.
+  ///     suspend.
   @MainActor
   public func send(
     _ action: ViewAction,
@@ -437,7 +437,7 @@ public final class ViewStore<ViewState, ViewAction> {
   /// ``send(_:while:)``.
   ///
   /// - Parameter predicate: A predicate on `ViewState` that determines for how long this method
-  ///                        should suspend.
+  ///   should suspend.
   @MainActor
   public func yield(while predicate: @escaping (ViewState) -> Bool) async {
     if #available(iOS 15, macOS 12, tvOS 15, watchOS 8, *) {
